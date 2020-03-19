0fcd081c80a9edf5a76c06a05825015ad175ec97
20399 <yanfang.lin@hand-china.com>
2020-03-19 09:39:36 +0800

    [IMP] 创建项目时添加普通敏捷项目类型

--- react/containers/components/c7n/routes/projects/constant.js
+++ react/containers/components/c7n/routes/projects/constant.js
@@ -1,11 +1,11 @@
 const HAS_AGILE_PRO = C7NHasModule('@choerodon/agile-pro');
 const PROJECT_TYPE = HAS_AGILE_PRO ? {
-  GENERAL: '普通应用项目',
+  GENERAL: 'DevOps全流程项目',
   // LOWCODE: '低代码应用项目',
-  // AGILE: '敏捷管理项目',
+  AGILE: '普通敏捷项目',
   PROGRAM: '敏捷项目群项目',
 } : {
-  GENERAL: '普通应用项目',
+  GENERAL: 'DevOps全流程项目',
 };
 
 export { PROJECT_TYPE, HAS_AGILE_PRO };
