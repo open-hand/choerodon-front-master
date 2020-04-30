c99edde1908a963fb3d6ef81140a8a011fe0d248
isaac <273070111@qq.com>
2020-04-30 14:31:50 +0800

    [ADD]修改星标项目

--- react/containers/components/c7n/routes/workBench/components/StarTargetPro/index.js
+++ react/containers/components/c7n/routes/workBench/components/StarTargetPro/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Button } from 'choerodon-ui/pro';
+import emptyImg from '../../../../../../images/owner.png';
 
 import './index.less';
 
 const StarTargetPro = () => (
   <div className="c7n-starTargetPro">
-    星标项目
+    <p className="c7n-starTargetPro-name">星标项目</p>
+    <div className="c7n-starTargetPro-content">
+      <img src={emptyImg} alt="empty"/>
+      <div className="c7n-starTargetPro-content-emptyText">
+        <p className="c7n-starTargetPro-content-emptyText-emptyP">暂无星标</p>
+        <p className="c7n-starTargetPro-content-emptyText-emptySuggest">您还没有星标项目，请前往"项目管理"页面进行添加</p>
+        <Button funcType="raised" color="primary">转到项目管理</Button>
+      </div>
+    </div>
   </div>
 )
 
