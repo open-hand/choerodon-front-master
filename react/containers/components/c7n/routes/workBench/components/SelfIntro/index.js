35e7379ae7234c4bb3f0b4f33fa426c4ab42a880
20399 <yanfang.lin@hand-china.com>
2020-05-06 18:02:29 +0800

    [IMP] 工作台个人信息内容优化

--- react/containers/components/c7n/routes/workBench/components/SelfIntro/index.js
+++ react/containers/components/c7n/routes/workBench/components/SelfIntro/index.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Icon } from 'choerodon-ui/pro';
+import { useWorkBenchStore } from '../../stores';
 
 import './index.less';
 
-const SelfIntro = () => (
-  <div className="c7n-selfIntro">
-    hi
-  </div>
-)
+const SelfIntro = () => {
+  const {
+    AppState: { getUserInfo },
+  } = useWorkBenchStore();
+
+  const {
+    imageUrl,
+    realName,
+    loginName,
+    email,
+  } = getUserInfo || {};
+
+  useEffect(() => {
+
+  }, []);
+  
+  return (
+    <div className="c7n-selfInfo">
+      <div className="c7n-selfInfo-title">
+        {imageUrl ? (
+          <img
+            src={imageUrl}
+            className="c7n-selfInfo-head"
+            alt=""
+          />
+        ) : (
+          <span className="c7n-selfInfo-head">
+            {(realName || '').substring(0, 1).toUpperCase()}
+          </span>
+        )}
+        <span className="c7n-selfInfo-name">Hi，{realName}</span>
+      </div>
+      <div className="c7n-selfInfo-email">
+        <Icon type="email-o" />
+        <span>{email}</span>
+      </div>
+    </div>
+  );
+};
 
 export default SelfIntro;
