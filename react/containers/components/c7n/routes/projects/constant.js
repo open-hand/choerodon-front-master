4958a2a3bf63ff60eb5b8ca415d8496ec98fd0b5
20399 <yanfang.lin@hand-china.com>
2020-05-07 11:13:42 +0800

    [IMP] 创建项目时添加运维项目变量

--- react/containers/components/c7n/routes/projects/constant.js
+++ react/containers/components/c7n/routes/projects/constant.js
@@ -4,6 +4,7 @@ const PROJECT_TYPE = HAS_AGILE_PRO ? {
   // LOWCODE: '低代码应用项目',
   AGILE: '普通敏捷项目',
   PROGRAM: '敏捷项目群项目',
+  OPERATIONS: '运维项目',
 } : {
   GENERAL: 'DevOps全流程项目',
   AGILE: '普通敏捷项目',
