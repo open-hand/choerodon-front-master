a69544fff0ffc47b8160045d4976cb07f2b3e558
isaac <273070111@qq.com>
2020-06-10 09:45:50 +0800

    [ADD]增加工作台功能

--- react/containers/components/c7n/routes/workBench/components/StarTargetPro/StarTargetPro.js
+++ react/containers/components/c7n/routes/workBench/components/StarTargetPro/StarTargetPro.js
@@ -8,16 +8,33 @@ import './index.less';
 const init = [{
   text: '基础架构管理-区块链中…(rdm01)',
   project: 'DevOps全流程项目',
+  id: 1,
+  time: '2018-07-07',
 }, {
   text: '基础架构管理-区块链中…(rdm01)',
   project: 'DevOps全流程项目',
+  id: 2,
+  time: '2018-07-07',
 }, {
   text: '基础架构管理-区块链中…(rdm01)',
   project: 'DevOps全流程项目',
+  id: 3,
+  time: '2018-07-07',
 }];
 
 const StarTargetPro = () => {
   const [starProjects, setStarProjects] = useState([]);
+
+  const handleClickItem = (s) => {
+    setStarProjects(starProjects.map(si => {
+     if (si.id === s.id) {
+       si.active = true;
+     } else {
+       si.active = false;
+     }
+     return si;
+    }))
+  }
 
   const renderContent = () => {
     if (starProjects.length == 0) {
@@ -36,14 +53,40 @@ const StarTargetPro = () => {
         <div className="c7n-starTargetPro-proContainer">
           {
             starProjects.map(s => (
-              <div className="c7n-starTargetPro-proContainer-items">
+              <div
+                className={s.active ? "c7n-starTargetPro-proContainer-items c7n-starTargetPro-proContainer-focus" : "c7n-starTargetPro-proContainer-items"}
+                onClick={() => handleClickItem(s)}
+              >
+                {
+                  s.active && (
+                    <div className="c7n-starTargetPro-proContainer-items-correct">
+                      <i className="c7n-starTargetPro-proContainer-items-correct-icon" />
+                    </div>
+                  )
+                }
                 <div className="c7n-starTargetPro-proContainer-items-icon" />
-                <p className="c7n-starTargetPro-proContainer-items-text">{s.text}</p>
-                <p className="c7n-starTargetPro-proContainer-items-project">{s.project}
-                  <span>
-                    <Icon type="trending_flat" />
+                <p style={{ color: s.active ? 'white' : 'rgba(58,52,95,1)' }} className="c7n-starTargetPro-proContainer-items-text">{s.text}</p>
+                <p style={{ color: s.active ? 'white' : 'rgba(58,52,95,1)' }} className="c7n-starTargetPro-proContainer-items-project">{s.project}
+                  <span
+                    style={{
+                      position: s.active ? 'relative' : 'unset',
+                      top: s.active ? '40px' : 'unset'
+                    }}
+                  >
+                    <Icon style={{ color: s.active ? 'white' : '#6887E8' }} type="trending_flat" />
                   </span>
                 </p>
+                {
+                  s.active && (
+                    <div className="c7n-starTargetPro-proContainer-items-extra">
+                      <div className="c7n-starTargetPro-proContainer-items-extra-icon" />
+                      <div className="c7n-starTargetPro-proContainer-items-extra-text">
+                        <p>创建于</p>
+                        <p>{s.time}</p>
+                      </div>
+                    </div>
+                  )
+                }
               </div>
             ))
           }
