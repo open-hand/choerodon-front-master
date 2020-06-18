f032a2dd5b9692f784e13501602a9cc6d7db8999
20399 <yanfang.lin@hand-china.com>
2020-06-18 14:09:00 +0800

    [IMP] 工作台待审核、应用服务模块添加接口

--- react/containers/components/c7n/routes/workBench/components/TodoThings/index.js
+++ react/containers/components/c7n/routes/workBench/components/TodoThings/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import Card from '../card';
 import Check from '../check';
-import Doc from '../doc';
+import { useWorkBenchStore } from '../../stores';
 
 import './index.less';
 
-const StarTargetPro = () => (
-  <div className="c7n-workbench-todo">
-    <Card
-      title="待审核"
-      className="c7n-workbench-check"
-    >
-      <Check />
-    </Card>
-    {/*<Card*/}
-    {/*  title="文档"*/}
-    {/*  showLink*/}
-    {/*  pathname="/ddd"*/}
-    {/*  className="c7n-workbench-doc"*/}
-    {/*>*/}
-    {/*  <Doc />*/}
-    {/*</Card>*/}
-  </div>
-);
+const StarTargetPro = observer(() => {
+  const {
+    auditDs,
+  } = useWorkBenchStore();
+  
+  return (
+    <div className="c7n-workbench-todo">
+      <Card
+        title="待审核"
+        showCount
+        count={auditDs.length}
+        className="c7n-workbench-check"
+      >
+        <Check />
+      </Card>
+    </div>
+  );
+});
 
 export default StarTargetPro;
--- react/containers/components/c7n/routes/workBench/components/check/index.js
+++ react/containers/components/c7n/routes/workBench/components/check/index.js
@@ -1,64 +1,60 @@
 import React from 'react';
-import map from 'lodash/map';
+import { observer } from 'mobx-react-lite';
 import { Icon } from 'choerodon-ui/pro';
+import { useWorkBenchStore } from '../../stores';
+import LoadingBar from '../../../../tools/loading-bar';
 
 import './index.less';
 
-const data = [
-  {
-    id: 1,
-    projectName: '基础架构管理-区块链中台研发',
-    content: '流水线“持续交付流水线”目前暂停于【开发环境】阶段的打法上发放',
-    type: 'pipeline',
-  },
-  {
-    id: 2,
-    projectName: '基础架构管理-区块链中台研发',
-    content: '李丹丹（9211）在应用服务”XXX“中提交了合并请',
-    type: 'merge',
-    userName: '李丹丹',
-    userUrl: '',
-  },
-  {
-    id: 3,
-    projectName: '基础架构管理-区块链中台研发',
-    content: '流水线“持续交付流水线”目前暂停于【开发环境】阶段的打法上发放',
-    type: 'pipeline',
-  },
-  {
-    id: 4,
-    projectName: '基础架构管理-区块链中台研发',
-    content: '李丹丹（9211）在应用服务”XXX“中提交了合并请',
-    type: 'merge',
-    userName: '李丹丹',
-    userUrl: '',
-  },
-];
+const StarTargetPro = observer(() => {
+  const {
+    auditDs,
+    history,
+    AppState: { currentMenuType: { organizationId, name } },
+  } = useWorkBenchStore();
 
-const StarTargetPro = () => (map(data, ({ id, projectName, content, userName, userUrl }) => (
-  <div className="c7n-workbench-check-item" key={id}>
-    <div className="c7n-workbench-check-item-border" />
-    <div className="c7n-workbench-check-item-content">
-      <div className="c7n-workbench-check-item-project">{projectName}</div>
-      <div className="c7n-workbench-check-item-des">
-        {userName && (
-          <div className="c7n-workbench-check-item-user">
-            {userUrl ? (
-              <img src={userUrl} alt="avatar" />) : (
-                <span className="c7n-workbench-check-item-user-text">
-                  {(userName || '').toUpperCase().substring(0, 1)}
-                </span>
-            )}
+  function linkToDetail(record) {
+    const { type, projectId, projectName, pipelineRecordId, gitlabUrl } = record.toData() || {};
+    if (type === 'pipeline') {
+      const search = `?id=${projectId}&name=${projectName}&organizationId=${organizationId}&type=project`;
+      history.push(`/devops/deployment-operation${search}&pipelineRecordId=${pipelineRecordId}`);
+    } else if (type === 'merge_request') {
+      window.open(gitlabUrl);
+    }
+  }
+
+  if (!auditDs || auditDs.status === 'loading') {
+    return <LoadingBar display />;
+  }
+
+  return (auditDs.map((record) => {
+    const { projectName, content, imageUrl, type } = record.toData() || {};
+    return (
+      <div className="c7n-workbench-check-item" key={record.id}>
+        <div className="c7n-workbench-check-item-border" />
+        <div className="c7n-workbench-check-item-content">
+          <div className="c7n-workbench-check-item-project">{projectName}</div>
+          <div className="c7n-workbench-check-item-des">
+            {type === 'merge_request' ? (
+              <div className="c7n-workbench-check-item-user">
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" />) : (
+                    <span className="c7n-workbench-check-item-user-text">
+                      {(content || '').toUpperCase().substring(0, 1)}
+                    </span>
+                )}
+              </div>
+            ) : null}
+            <span className="c7n-workbench-check-item-des-text">{content}</span>
           </div>
-        )}
-        <span className="c7n-workbench-check-item-des-text">{content}</span>
+        </div>
+        <div className="c7n-workbench-check-item-btn" onClick={() => linkToDetail(record)}>
+          <Icon type="find_in_page" className="c7n-workbench-check-item-btn-icon" />
+          <span>详情</span>
+        </div>
       </div>
-    </div>
-    <div className="c7n-workbench-check-item-btn">
-      <Icon type="find_in_page" className="c7n-workbench-check-item-btn-icon" />
-      <span>详情</span>
-    </div>
-  </div>
-)));
+    );
+  }));
+});
 
 export default StarTargetPro;
