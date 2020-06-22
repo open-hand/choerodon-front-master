5cb78bad0b736fc7778208df5c2f93e31b2febbe
20399 <yanfang.lin@hand-china.com>
2020-06-22 17:17:46 +0800

    [IMP] 修改工作台空界面样式

--- react/containers/components/c7n/routes/workBench/stores/QuestionDataSet.js
+++ react/containers/components/c7n/routes/workBench/stores/QuestionDataSet.js
@@ -5,9 +5,12 @@ export default (({ organizationId }) => ({
   idField: 'issueId',
   parentField: 'parentId',
   transport: {
-    read: {
-      url: `agile/v1/organizations/${organizationId}/work_bench/personal/backlog_issues`,
-      method: 'post',
+    read: ({ data }) => {
+      const { projectId, page = 0 } = data || {};
+      return ({
+        url: `agile/v1/organizations/${organizationId}/work_bench/personal/backlog_issues?page=${page}&size=20${projectId ? `projectId=${projectId}` : ''}`,
+        method: 'post',
+      });
     },
   },
   // fields: [],
