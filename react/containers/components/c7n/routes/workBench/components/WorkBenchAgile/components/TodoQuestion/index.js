dd5dbf806b9d528455057371e7934c82494f492a
20399 <yanfang.lin@hand-china.com>
2020-06-22 14:48:23 +0800

    [IMP] 修改工作台内容

--- react/containers/components/c7n/routes/workBench/components/WorkBenchAgile/components/TodoQuestion/index.js
+++ react/containers/components/c7n/routes/workBench/components/WorkBenchAgile/components/TodoQuestion/index.js
@@ -1,71 +1,136 @@
 import React, { useState } from 'react';
-import { Icon } from 'choerodon-ui';
+import { Icon, Tooltip, Tree } from 'choerodon-ui/pro';
 import { observer } from 'mobx-react-lite';
 import Card from '../../../card';
+import { useWorkBenchStore } from '../../../../stores';
+import EmptyPage from '../../../empty-page';
+import LoadingBar from '../../../../../../tools/loading-bar';
 
 import './index.less';
 
 const TodoQuestion = observer(() => {
-  const [questions, setQuestions] = useState([{
-    project: '基础架构管理-区块链中台研发',
-    issueType: 'story',
-    issueId: 'C7NCD123',
-    description: '开发人员在创建CI流水线时，能添加与编辑Npm构建的步骤',
-    status: '待处理',
-    priority: '中',
-  }, {
-    project: '基础架构管理-区块链中台研发',
-    issueType: 'story',
-    issueId: 'C7NCD123',
-    description: '开发人员在创建CI流水线时，能添加与编辑Npm构建的步骤',
-    status: '待处理',
-    priority: '中',
-  }]);
+  const {
+    AppState: { currentMenuType: { organizationId } },
+    questionDs,
+    history,
+  } = useWorkBenchStore();
 
-  const renderIssues = () => {
-    const mapping = {
-      story: {
-        icon: 'agile_story',
-        color: '#00BFA5'
+  function handleClick(record) {
+    const { projectVO, issueId } = record.toData();
+    const { id: projectId, name: projectName } = projectVO || {};
+    history.push({
+      pathname: '/agile/scrumboard',
+      search: `?id=${projectId}&name=${projectName}&organizationId=${organizationId}&type=project`,
+      state: {
+        issueId,
       },
-      bug: {
-        icon: 'agile_fault',
-        color: '#F77A70',
-      },
-      task: {
-        icon: 'agile_task',
-        color: '#6887E8'
-      },
-    };
-    return questions.map(q => (
-      <div className="c7n-todoQuestion-issueContent-issueItem">
-        <p className="c7n-todoQuestion-issueContent-issueItem-project">{q.project}</p>
+    });
+  }
+  
+  function getIssueType(typeCode) {
+    let mes = '';
+    let icon = '';
+    let color = '';
+    switch (typeCode) {
+      case 'story':
+        mes = '故事';
+        icon = 'agile_story';
+        color = '#00bfa5';
+        break;
+      case 'bug':
+        mes = '缺陷';
+        icon = 'agile_fault';
+        color = '#f44336';
+        break;
+      case 'issue_epic':
+        mes = '史诗';
+        icon = 'agile_epic';
+        color = '#743be7';
+        break;
+      case 'sub_task':
+        mes = '子任务';
+        icon = 'agile_subtask';
+        color = '#4d90fe';
+        break;
+      default:
+        mes = '任务';
+        icon = 'agile_task';
+        color = '#4d90fe';
+    }
+
+    return (
+      <Tooltip title={mes} placement="top">
+        <Icon
+          className="c7n-todoQuestion-issueContent-issueItem-main-icon"
+          type={icon}
+          style={{ color }}
+        />
+      </Tooltip>
+    );
+  }
+
+  function getStatus(statusVO) {
+    const { type, name } = statusVO || {};
+    return (
+      <span
+        className={`c7n-todoQuestion-issueContent-issueItem-main-status c7n-todoQuestion-issueContent-issueItem-main-status-${type}`}
+      >
+        {name}
+      </span>
+    );
+  }
+  
+  function nodeRenderer({ record }) {
+    const { projectVO, typeCode, issueNum, summary, priorityVO, statusVO } = record.toData() || {};
+    return (
+      <div className="c7n-todoQuestion-issueContent-issueItem" onClick={() => handleClick(record)}>
+        <p className="c7n-todoQuestion-issueContent-issueItem-project">{projectVO ? projectVO.name : ''}</p>
         <div className="c7n-todoQuestion-issueContent-issueItem-main">
-          <Icon
-            className="c7n-todoQuestion-issueContent-issueItem-main-icon"
-            type={mapping[q.issueType].icon}
-            style={{ color: mapping[q.issueType].color }}
-          />
-          <span className="c7n-todoQuestion-issueContent-issueItem-main-issueId">{q.issueId}</span>
-          <span className="c7n-todoQuestion-issueContent-issueItem-main-description">{q.description}</span>
-          <span className="c7n-todoQuestion-issueContent-issueItem-main-status">{q.status}</span>
+          {getIssueType(typeCode)}
+          <span className="c7n-todoQuestion-issueContent-issueItem-main-issueId">{issueNum}</span>
+          <Tooltip title={summary} placement="top">
+            <span className="c7n-todoQuestion-issueContent-issueItem-main-description">{summary}</span>
+          </Tooltip>
+          {getStatus(statusVO)}
           <span className="c7n-todoQuestion-issueContent-issueItem-main-priority">
-            {q.priority}
+            {priorityVO ? priorityVO.name : '无'}
           </span>
         </div>
       </div>
-    ));
-  };
+    );
+  }
+
+  function getContent() {
+    if (!questionDs || questionDs.status === 'loading') {
+      return <LoadingBar display />;
+    }
+    if (!questionDs.length) {
+      return (
+        <EmptyPage
+          title="暂无待办问题"
+          describe="当前迭代暂无待办问题"
+        />
+      );
+    }
+    return (
+      <Tree
+        dataSet={questionDs}
+        renderer={nodeRenderer}
+        // onExpand={handleExpanded}
+        className="c7n-todoQuestion-issueContent"
+      />
+    );
+  }
 
   return (
     <div className="c7n-todoQuestion">
       <Card
         title="待办问题"
         showCount
-        count={questions.length}
+        count={questionDs ? questionDs.length : 0}
         className="c7n-todoQuestion-issueContent"
       >
-        {renderIssues()}
+        {getContent()}
       </Card>
     </div>
   );
--- react/containers/components/c7n/routes/workBench/stores/index.js
+++ react/containers/components/c7n/routes/workBench/stores/index.js
@@ -6,6 +6,7 @@ import { DataSet } from 'choerodon-ui/pro';
 import AuditDataSet from './AuditDataSet';
 import useStore from './useStore';
 import AppServiceDataSet from './AppServiceDataSet';
+import QuestionDataSet from './QuestionDataSet';
 
 const Store = createContext();
 
@@ -23,24 +24,29 @@ export const StoreProvider = withRouter(inject('AppState')(observer((props) => {
   const workBenchUseStore = useStore(history);
   const auditDs = useMemo(() => new DataSet(AuditDataSet({ organizationId })), [organizationId]);
   const appServiceDs = useMemo(() => new DataSet(AppServiceDataSet({ organizationId })), [organizationId]);
+  const questionDs = useMemo(() => new DataSet(QuestionDataSet({ organizationId })), [organizationId]);
 
   useEffect(() => {
     const project = workBenchUseStore.getActiveStarProject;
     if (project && project.id) {
-      auditDs.setQueryParameter('projectId', project.id);
-      appServiceDs.setQueryParameter('projectId', project.id);
+      auditDs.setQueryParameter('project_id', project.id);
+      appServiceDs.setQueryParameter('project_id', project.id);
+      questionDs.setQueryParameter('projectId', project.id);
     } else {
-      auditDs.setQueryParameter('projectId', null);
-      appServiceDs.setQueryParameter('projectId', null);
+      auditDs.setQueryParameter('project_id', null);
+      appServiceDs.setQueryParameter('project_id', null);
+      questionDs.setQueryParameter('projectId', null);
     }
     auditDs.query();
     appServiceDs.query();
+    questionDs.query();
   }, [workBenchUseStore.getActiveStarProject]);
 
   const value = {
     ...props,
     auditDs,
     appServiceDs,
+    questionDs,
     workBenchUseStore,
   };
 
