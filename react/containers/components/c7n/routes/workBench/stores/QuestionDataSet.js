97d35c8aca3a9a6fe7d5e2b1d733d1df897cdaa7
20399 <yanfang.lin@hand-china.com>
2020-06-22 21:41:54 +0800

    [IMP] 修改工作台待办问题请求接口

--- react/containers/components/c7n/routes/workBench/stores/QuestionDataSet.js
+++ react/containers/components/c7n/routes/workBench/stores/QuestionDataSet.js
@@ -8,7 +8,7 @@ export default (({ organizationId }) => ({
     read: ({ data }) => {
       const { projectId, page = 0 } = data || {};
       return ({
-        url: `agile/v1/organizations/${organizationId}/work_bench/personal/backlog_issues?page=${page}&size=20${projectId ? `projectId=${projectId}` : ''}`,
+        url: `agile/v1/organizations/${organizationId}/work_bench/personal/backlog_issues?page=${page}&size=20${projectId ? `&projectId=${projectId}` : ''}`,
         method: 'post',
       });
     },
