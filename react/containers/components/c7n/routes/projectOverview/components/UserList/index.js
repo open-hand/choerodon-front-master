c8b12e5ed5c72ce20a372e26981e36b52aee43f2
20399 <yanfang.lin@hand-china.com>
2020-07-12 15:11:27 +0800

    [IMP] 项目概览活跃人员接口修改

--- react/containers/components/c7n/routes/projectOverview/components/UserList/index.js
+++ react/containers/components/c7n/routes/projectOverview/components/UserList/index.js
@@ -1,6 +1,6 @@
 import React, { useState, memo, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Button, Tooltip, Select } from 'choerodon-ui/pro';
+import { Pagination, Tooltip, Select } from 'choerodon-ui/pro';
 import { useProjectOverviewStore } from '../../stores';
 import OverviewWrap from '../OverviewWrap';
 
@@ -9,36 +9,40 @@ import './index.less';
 const UserList = () => {
   const {
     userListDs,
+    projectOverviewStore,
   } = useProjectOverviewStore();
   const clsPrefix = 'c7n-project-overview-user-list';
 
   return (
     <OverviewWrap height={459}>
-      <OverviewWrap.Header title={`活跃成员(${0})`} />
-      {userListDs.map((record) => {
-        const { imageUrl, loginName, realName, roles } = record.toData();
-        return (
-          <div className={`${clsPrefix}-item`} key={record.id}>
-            <Tooltip title={`${realName} (${loginName})`} placement="top">
-              <span
-                className={`${clsPrefix}-item-avatar`}
-                style={{
-                  backgroundImage: imageUrl ? `url(${imageUrl})` : '',
-                }}
-              >
-                {!imageUrl && realName && realName.slice(0, 1)}
-              </span>
-            </Tooltip>
-            <span className={`${clsPrefix}-item-name`}>{realName}</span>
-            <span className={`${clsPrefix}-item-status`}>活跃</span>
-            <div className={`${clsPrefix}-item-roles`}>
-              {roles.map(({ name: roleName }) => (
-                <span className={`${clsPrefix}-item-roles-item`}>{roleName}</span>
-              ))}
+      <OverviewWrap.Header title={`活跃成员(${projectOverviewStore.getTotalOnlineUser})`} />
+      <div className={`${clsPrefix}-content`}>
+        {userListDs.map((record) => {
+          const { imageUrl, loginName, realName, roleNames } = record.toData();
+          return (
+            <div className={`${clsPrefix}-item`} key={record.id}>
+              <Tooltip title={`${realName} (${loginName})`} placement="top">
+                <span
+                  className={`${clsPrefix}-item-avatar`}
+                  style={{
+                    backgroundImage: imageUrl ? `url(${imageUrl})` : '',
+                  }}
+                >
+                  {!imageUrl && realName && realName.slice(0, 1)}
+                </span>
+              </Tooltip>
+              <span className={`${clsPrefix}-item-name`}>{realName}</span>
+              <span className={`${clsPrefix}-item-status`}>活跃</span>
+              <div className={`${clsPrefix}-item-roles`}>
+                {(roleNames.split(',') || []).map((item) => (
+                  <span className={`${clsPrefix}-item-roles-item`}>{item}</span>
+                ))}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <Pagination dataSet={userListDs} className={`${clsPrefix}-pagination`} />
     </OverviewWrap>
   );
 };
