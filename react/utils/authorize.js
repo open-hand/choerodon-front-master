818088c64a94d44ca2395a4e8808f4df6a2e61df
laincarl <1047972853@qq.com>
2020-07-28 09:59:29 +0800

    [FIX]修复重定向地址

--- react/utils/authorize.js
+++ react/utils/authorize.js
@@ -27,6 +27,8 @@ export function authorizeC7n() {
   // 这里是为了告诉oauth我要重定向的uri是什么，必须和client中对应，跳转到非client的页面会报错。
   if (window.location.href.indexOf('#') === -1) {
     window.location = `${AUTH_URL}&redirect_uri=${uri}`;
+  } else if (window.location.href.indexOf('?') === -1) {
+    window.location = `${AUTH_URL}&redirect_uri=${uri}%3FredirectFlag`;
   } else {
     window.location = `${AUTH_URL}&redirect_uri=${uri}%26redirectFlag`;
   }
