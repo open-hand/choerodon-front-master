006b9948276ebc369504efffdc2d1dc7ad243236
NollieLeo <736653759@qq.com>
2020-08-05 16:12:40 +0800

    [FIX] 修复预览环境样式冲突导致项目预览界面样式出错问题

--- react/containers/components/c7n/tools/loading-bar/index.js
+++ react/containers/components/c7n/tools/loading-bar/index.js
@@ -10,12 +10,12 @@ const LoadingBarRequiredProps = {
 
 function LoadingBar({ display = false }) {
   const spinClass = classNames({
-    'spin-hidden': !display,
-    'spin-container': display,
+    'c7n-spin-hidden': !display,
+    'c7n-spin-container': display,
   });
   return (
     <div className={spinClass}>
-      <Progress type="loading" />
+      <Progress type="loading" className="c7n-spin-container-progress" />
     </div>
   );
 }
