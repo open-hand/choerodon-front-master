4b93d501911683ce8e2881b02a488bcc18aaa475
laincarl <1047972853@qq.com>
2020-08-05 17:07:03 +0800

    [FIX]更改路由替换

--- react/routes.js
+++ react/routes.js
@@ -3,17 +3,11 @@ import { Route } from 'react-router-dom';
 import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
 import nomatch from './containers/components/c7n/tools/error-pages/404';
 
-const routes = JSON.parse(process.env.ROUTES).map((route) => (
-  <Route
-    path={route.key}
-    component={React.lazy(route.path)}
-  />
-));
-// console.log(process.env.ROUTES);
 const AutoRouter = () => (
   <Suspense fallback={<span />}>
     <CacheSwitch>
-      {routes}
+      {/* eslint-disable-next-line no-undef */}
+      {__ROUTES__}
       <CacheRoute path="*" component={nomatch} />
     </CacheSwitch>
   </Suspense>
