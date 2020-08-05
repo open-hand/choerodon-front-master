e9fa4e51a3e86baf3ffb46519a0d154c49616c5b
laincarl <1047972853@qq.com>
2020-08-05 17:21:14 +0800

    [IMP]更新react-intl

--- react/config.js
+++ react/config.js
@@ -1,6 +1,7 @@
 const alias = require('../alias').webpack;
 
 module.exports = {
+  port: 9092,
   webSocketServer: 'ws://notify.staging.saas.hand-china.com',
   master: './react/master.js',
   projectType: 'choerodon',
