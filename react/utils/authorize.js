43173fa50ec97b01864eb90a3398895528cd4235
laincarl <1047972853@qq.com>
2020-09-01 09:55:55 +0800

    [FIX]更改authorize重定向地址redirectFlag的判断

--- react/utils/authorize.js
+++ react/utils/authorize.js
@@ -23,17 +23,16 @@ export function logout() {
 
 export function authorizeC7n() {
   // 为了把这个hash传到oauth里要把#换成%23
-  const uri = escape(`${window.location.origin}/#${sessionStorage.getItem('historyPath') || '/'}`);
-
+  const uri = `${window.location.origin}/#${sessionStorage.getItem('historyPath') || '/'}`;
+  if (uri.indexOf('?') > 0) {
+    url += '&redirectFlag'
+  } else {
+    url += '?redirectFlag'
+  }
+  const redirect_uri = escape(uri)
   window.localStorage.removeItem('lastClosedId');
   // 这里是为了告诉oauth我要重定向的uri是什么，必须和client中对应，跳转到非client的页面会报错。
-  if (window.location.href.indexOf('#') === -1) {
-    window.location = `${AUTH_URL}&redirect_uri=${uri}`;
-  } else if (window.location.href.indexOf('?') === -1) {
-    window.location = `${AUTH_URL}&redirect_uri=${uri}%3FredirectFlag`;
-  } else {
-    window.location = `${AUTH_URL}&redirect_uri=${uri}%26redirectFlag`;
-  }
+  window.location = `${AUTH_URL}&redirect_uri=${redirect_uri}`;
 }
 
 export function authorizeUrl() {
