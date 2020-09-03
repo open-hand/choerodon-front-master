1bd278c7d66beffa3372a81da11f1ce0403ca1d3
NollieLeo <736653759@qq.com>
2020-09-03 09:38:15 +0800

    [FIX] 快速链接中，点击修改后，项目名称问题

--- react/containers/components/c7n/routes/workBench/components/WorkBenchAgile/components/QuickLink/stores/addLinkDataSet.js
+++ react/containers/components/c7n/routes/workBench/components/WorkBenchAgile/components/QuickLink/stores/addLinkDataSet.js
@@ -1,3 +1,14 @@
+const isJsonString = (str) => {
+  try {
+    if (typeof JSON.parse(str) == 'object') {
+      return true;
+    }
+  // eslint-disable-next-line no-empty
+  } catch (e) {
+  }
+  return false;
+};
+
 export default (AppState, detail) => ({
   autoCreate: true,
   fields: [{
@@ -6,46 +17,44 @@ export default (AppState, detail) => ({
     defaultValue: 'project',
   }, {
     name: 'projectId',
-    type: 'string',
+    type: 'number',
     label: '项目',
     textField: 'name',
     valueField: 'id',
     dynamicProps: {
       required: ({ record }) => record.get('scope') === 'project',
     },
-    lookupAxiosConfig: (data) => {
-      return ({
-        method: 'get',
-        url: `/iam/choerodon/v1/organizations/${AppState.currentMenuType.organizationId}/users/${AppState.getUserId}/projects/paging?page=0&size=10`,
-        data: {
-          param: [],
-          searchParams: {
-            name: data.params.name,
-          }
+    lookupAxiosConfig: (data) => ({
+      method: 'get',
+      url: `/iam/choerodon/v1/organizations/${AppState.currentMenuType.organizationId}/users/${AppState.getUserId}/projects/paging?page=0&size=10`,
+      data: {
+        param: [],
+        searchParams: {
+          name: data.params.name,
         },
-        transformResponse: (res) => {
-          let newRes;
-          try {
-            newRes = JSON.parse(res);
-            if (newRes.content.length % 10 === 0 && newRes.content.length !== 0) {
-              newRes.content.push({
-                id: 'more',
-                name: '加载更多',
-              });
-            }
-            if (detail?.projectId && !newRes.content.some(n => n.id === detail.projectId)) {
-              newRes.content.unshift({
-                id: detail.projectId,
-                name: detail.projectName,
-              })
-            }
-            return newRes;
-          } catch (e) {
-            throw new Error(e);
+      },
+      transformResponse: (res) => {
+        let newRes;
+        try {
+          newRes = isJsonString(res) ? JSON.parse(res) : res;
+          if (newRes.content.length % 10 === 0 && newRes.content.length !== 0) {
+            newRes.content.push({
+              id: 'more',
+              name: '加载更多',
+            });
           }
-        },
-      })
-    }
+          if (detail?.projectId && !newRes.content.some((n) => n.id === detail.projectId)) {
+            newRes.content.unshift({
+              id: detail.projectId,
+              name: detail.projectName,
+            });
+          }
+          return newRes;
+        } catch (e) {
+          throw new Error(e);
+        }
+      },
+    }),
   }, {
     name: 'name',
     type: 'string',
@@ -57,5 +66,5 @@ export default (AppState, detail) => ({
     type: 'string',
     label: '链接地址',
     required: true,
-  }]
-})
+  }],
+});
