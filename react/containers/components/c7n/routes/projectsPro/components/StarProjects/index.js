09880fefde3a960f74584e822e55619440cdde46
NollieLeo <736653759@qq.com>
2020-10-14 14:21:55 +0800

    [ADD] 前端增加星标项目调整顺序的操作逻辑

--- react/containers/components/c7n/routes/projectsPro/components/StarProjects/index.js
+++ react/containers/components/c7n/routes/projectsPro/components/StarProjects/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import { Icon } from 'choerodon-ui';
 import { observer } from 'mobx-react-lite';
+import {
+  Droppable, Draggable, DragDropContext,
+} from 'react-beautiful-dnd';
 import { useProjectsProStore } from '../../stores';
 import ProjectTaskContent from '../projectTaskContent';
 
@@ -11,36 +14,61 @@ export default observer(() => {
     ProjectsProUseStore,
   } = useProjectsProStore();
 
-  const renderProjects = useCallback(() => ProjectsProUseStore.getStarProjectsList.map(p => (
-    <div
-      onClick={() => {
-        if (p.enabled) {
-          ProjectsProUseStore.handleClickProject(p);
-        }
-      }}
-      className="starProjects-items"
-      style={{
-        cursor: p.enabled ? 'pointer' : 'not-allowed',
-      }}
-    >
-      <div className="starProjects-items-topborder" />
-      <ProjectTaskContent alltrue data={p} />
-    </div>
+  const renderProjects = useCallback(() => ProjectsProUseStore.getStarProjectsList.map((p, index) => (
+    <Draggable key={`pre-${p.id}`} draggableId={`pre-${p.id}`} index={index}>
+      {
+        (dragProvided, snapshotinner) => (
+          <div
+            onClick={() => {
+              if (p.enabled) {
+                ProjectsProUseStore.handleClickProject(p);
+              }
+            }}
+            className="starProjects-items"
+            style={{
+              cursor: p.enabled ? 'pointer' : 'not-allowed',
+            }}
+            role="none"
+            ref={dragProvided.innerRef}
+            {...dragProvided.draggableProps}
+            {...dragProvided.dragHandleProps}
+          >
+            <div className="starProjects-items-topborder" />
+            <ProjectTaskContent alltrue data={p} />
+          </div>
+        )
+      }
+    </Draggable>
+
   )), [ProjectsProUseStore.getStarProjectsList]);
+
+  const onDragEnd = (data) => {
+    const { source, destination } = data;
+  };
 
   return (
     <div className="starProjects">
       <div className="starProjects-title-wrap">
-        <p className="starProjects-title">星标项目
-          <span>{
+        <p className="starProjects-title">
+          星标项目
+          <span>
+            {
             ProjectsProUseStore.getStarProjectsList.length || 0
           }
           </span>
         </p>
       </div>
-      <div className="starProjects-content-wrap">
-        {renderProjects()}
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="dropPreStep" isDropDisabled={false}>
+          {
+            (provided) => (
+              <div className="starProjects-content-wrap" ref={provided.innerRef}>
+                {renderProjects()}
+              </div>
+            )
+          }
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 });
