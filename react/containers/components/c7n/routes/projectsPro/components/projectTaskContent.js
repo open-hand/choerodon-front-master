d52c3973fd9fb38d2f8887afabfd2064c71803e3
isaac <273070111@qq.com>
2020-11-09 15:25:52 +0800

    [FIX]修改项目界面ui

--- react/containers/components/c7n/routes/projectsPro/components/projectTaskContent.js
+++ react/containers/components/c7n/routes/projectsPro/components/projectTaskContent.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { Icon } from 'choerodon-ui';
 import { Tooltip } from 'choerodon-ui/pro';
@@ -17,21 +18,47 @@ export default ({ data, alltrue }) => {
   return (
     <div>
       <div className="starProjects-items-content">
-        <div style={{ display: 'flex', alignItems: 'center', width: '60%' }}>
-          <div
-            className="starProjects-items-content-icon"
-            style={{
-              backgroundImage: data.imageUrl ? `url("${data.imageUrl}")` : getRandomBackground(data.id),
-            }}
-          >
-            {!data.imageUrl && data.name && data.name.slice(0, 1)}
-          </div>
-          <div className="starProjects-items-content-center">
+        <div
+          className="starProjects-items-content-icon"
+          style={{
+            backgroundImage: data.imageUrl ? `url("${data.imageUrl}")` : getRandomBackground(data.id),
+          }}
+        >
+          {!data.imageUrl && data.name && data.name.slice(0, 1)}
+        </div>
+        <div className="starProjects-rightContent">
+          <div className="starProjects-first">
             <p className="starProjects-items-content-center-title">
               <Tooltip title={data.name} placement="top">
                 {data.name}
               </Tooltip>
             </p>
+            <Icon
+              type={data.starFlag ? 'turned_in' : 'turned_in_not'}
+              style={{
+                color: data.starFlag ? 'rgb(86, 111, 225)' : 'rgb(196, 195, 225)',
+                fontSize: '20px',
+              }}
+              onClick={(e) => {
+                e.stopPropagation();
+                const { starFlag } = data;
+                ProjectsProUseStore.handleStarProject(data).then(() => {
+                  const allProjects = ProjectsProUseStore.getAllProjects;
+                  const index = allProjects.findIndex(i => String(i.id) === String(data.id));
+                  if (index || String(index) === '0') {
+                    ProjectsProUseStore.setAllProjects(allProjects.map((p, pI) => {
+                      if (String(pI) === String(index)) {
+                        p.starFlag = !starFlag;
+                      }
+                      return p;
+                    }));
+                  }
+                  ProjectsProUseStore.handleChangeStarProjects(data);
+                });
+              }}
+            />
+          </div>
+          <div className="starProjects-second">
             <div style={{ display: 'flex', alignItems: 'center', marginTop: 5 }}>
               <Tooltip title={data.code && data.code.toUpperCase()}>
                 <p className="starProjects-items-content-center-code">{data.code && data.code.toUpperCase()}</p>
@@ -39,57 +66,30 @@ export default ({ data, alltrue }) => {
               <p className={`starProjects-items-content-center-status starProjects-items-content-center-status-${data.enabled}`}>
                 {data.enabled ? '启用' : '停用'}
               </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="starProjects-items-content-right">
-          <Icon
-            type={data.starFlag ? 'turned_in' : 'turned_in_not'}
-            style={{
-              color: data.starFlag ? 'rgb(86, 111, 225)' : 'rgb(196, 195, 225)',
-              fontSize: '20px',
-            }}
-            onClick={(e) => {
-              e.stopPropagation();
-              const { starFlag } = data;
-              ProjectsProUseStore.handleStarProject(data).then(() => {
-                const allProjects = ProjectsProUseStore.getAllProjects;
-                const index = allProjects.findIndex(i => String(i.id) === String(data.id));
-                if (index || String(index) === '0') {
-                  ProjectsProUseStore.setAllProjects(allProjects.map((p, pI) => {
-                    if (String(pI) === String(index)) {
-                      p.starFlag = !starFlag;
-                    }
-                    return p;
-                  }));
-                }
-                ProjectsProUseStore.handleChangeStarProjects(data);
-              });
-            }}
-          />
-          <div className="starProjects-items-content-right-down">
-            <Tooltip title={data.createUserName} placement="top">
-              <div
-                className="starProjects-items-content-right-down-avatur"
-                style={{
-                  backgroundImage: `url(${data.createUserImageUrl})`,
-                }}
-              >
-                {!data.createUserImageUrl && data.createUserName && data.createUserName.slice(0, 1)}
+              <div className="starProjects-items-content-right-down">
+                <Tooltip title={data.createUserName} placement="top">
+                  <div
+                    className="starProjects-items-content-right-down-avatur"
+                    style={{
+                      backgroundImage: `url(${data.createUserImageUrl})`,
+                    }}
+                  >
+                    {!data.createUserImageUrl && data.createUserName && data.createUserName.slice(0, 1)}
+                  </div>
+                </Tooltip>
+                <p
+                  style={{
+                    marginTop: 7,
+                    fontSize: '12px',
+                    fontFamily: 'PingFangSC-Regular,PingFang SC',
+                    fontWeight: 400,
+                    color: 'rgba(58,52,95,0.65)',
+                  }}
+                >
+                  <TimePopover datetime={data.creationDate} />创建
+                </p>
               </div>
-            </Tooltip>
-            <p
-              style={{
-                marginTop: 7,
-                fontSize: '12px',
-                fontFamily: 'PingFangSC-Regular,PingFang SC',
-                fontWeight: 400,
-                color: 'rgba(58,52,95,0.65)',
-              }}
-            >
-              <TimePopover datetime={data.creationDate} />创建
-            </p>
+            </div>
           </div>
         </div>
       </div>
