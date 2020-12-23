448a3f131b0211d41448da8d020893be06465bb5
DingZhicheng <zhicheng.ding@hand-china.com>
2020-12-23 19:23:00 +0800

    [FIX]工作台我的事项空页面调整&修复文档更新人为空情况

--- react/containers/components/c7n/routes/workBench/components/doc/Doc.js
+++ react/containers/components/c7n/routes/workBench/components/doc/Doc.js
@@ -15,7 +15,9 @@ import { useWorkBenchStore } from '../../stores';
 
 const clsPrefix = 'c7n-workbench-doc';
 
-function Switch({ options: propsOption, children, onChange, defaultValue, checkedValue }) {
+function Switch({
+  options: propsOption, children, onChange, defaultValue, checkedValue,
+}) {
   const [value, setValue] = useState(defaultValue);
   const [options, setOptions] = useState(propsOption || []);
   const onClick = (v) => {
@@ -27,7 +29,7 @@ function Switch({ options: propsOption, children, onChange, defaultValue, checke
   useEffect(() => {
     if (!Array.isArray(options)) {
       setOptions([]);
-    } else if (!options.some(v => v.value)) {
+    } else if (!options.some((v) => v.value)) {
       setOptions(options.map((v, index) => ({ text: v, value: index })));
     }
     propsOption = options;
@@ -36,7 +38,7 @@ function Switch({ options: propsOption, children, onChange, defaultValue, checke
   return (
     <ul className={`${clsPrefix}-switch`}>
       {options.map((option, index) => (
-        <React.Fragment>
+        <>
           <li
             onClick={(e) => {
               e.preventDefault();
@@ -47,7 +49,7 @@ function Switch({ options: propsOption, children, onChange, defaultValue, checke
             {option.text || option}
           </li>
           <span className="line" />
-        </React.Fragment>
+        </>
       ))}
     </ul>
   );
@@ -77,11 +79,15 @@ const Doc = ({ history }) => {
       </div>
     );
   }
-  const goKnowledgeLink = ({ baseId, orgFlag, projectId, organizationId, spaceId, baseName, name }) => {
+  const goKnowledgeLink = ({
+    baseId, orgFlag, projectId, organizationId, spaceId, baseName, name,
+  }) => {
     const url = `/knowledge/${orgFlag ? 'organization' : 'project'}/doc/${baseId}?baseName=${baseName}&id=${orgFlag ? organizationId : projectId}&organizationId=${organizationId}&spaceId=${spaceId}&name=${name}&type=${orgFlag ? 'organization' : 'project'}`;
     history.push(url);
   };
-  const renderUserList = (userList, visibleText = false) => map(userList, ({ realName, loginName, email, ldap, imageUrl }) => (
+  const renderUserList = (userList, visibleText = false) => map(userList, ({
+    realName, loginName, email, ldap, imageUrl,
+  }) => (
     <Tooltip
       placement="top"
       title={ldap ? `${realName}(${loginName})` : `${realName}(${email})`}
@@ -104,49 +110,61 @@ const Doc = ({ history }) => {
   ));
 
   function renderItems() {
-    return map(docStore.getDocData, ({ knowledgeBaseName, orgFlag, id, baseId, organizationId, imageUrl, title, projectId, projectName, organizationName, updatedUserList, lastUpdateDate }) => (
-      <div className="c7n-workbench-doc-item" onClick={goKnowledgeLink.bind(this, { baseId, orgFlag, organizationId, spaceId: id, baseName: knowledgeBaseName, projectId, name: orgFlag ? organizationName : projectName })}>
-        <div className="c7n-workbench-doc-item-info">
-          <span className="c7n-workbench-doc-item-logo c7n-workbench-doc-item-logo-update">
-            {`${title}`.toUpperCase().substring(0, 1)}
-          </span>
-          <div className="c7n-workbench-doc-item-userlist">
-            {renderUserList(updatedUserList.slice(0, 3))}
-            {updatedUserList.length > 3 && (
-              <Tooltip
-                placement="top"
-                title={renderUserList(updatedUserList.slice(3), true)}
-              >
-                <span className="c7n-workbench-doc-item-userlist-user-item c7n-workbench-doc-item-userlist-user-item-more">
-                  +{updatedUserList.length - 3}
-                </span>
-              </Tooltip>
+    return map(docStore.getDocData, ({
+      knowledgeBaseName, orgFlag, id, baseId, organizationId, imageUrl, title, projectId, projectName, organizationName, updatedUserList: originUpdatedUserList, lastUpdateDate,
+    }) => {
+      const updatedUserList = originUpdatedUserList ? originUpdatedUserList.filter(Boolean) : [];
+      return (
+        <div
+          role="none"
+          className="c7n-workbench-doc-item"
+          onClick={goKnowledgeLink.bind(this, {
+            baseId, orgFlag, organizationId, spaceId: id, baseName: knowledgeBaseName, projectId, name: orgFlag ? organizationName : projectName,
+          })}
+        >
+          <div className="c7n-workbench-doc-item-info">
+            <span className="c7n-workbench-doc-item-logo c7n-workbench-doc-item-logo-update">
+              {`${title}`.toUpperCase().substring(0, 1)}
+            </span>
+            <div className="c7n-workbench-doc-item-userlist">
+              {renderUserList(updatedUserList.slice(0, 3))}
+              {updatedUserList.length > 3 && (
+                <Tooltip
+                  placement="top"
+                  title={renderUserList(updatedUserList.slice(3), true)}
+                >
+                  <span className="c7n-workbench-doc-item-userlist-user-item c7n-workbench-doc-item-userlist-user-item-more">
+                    +
+                    {updatedUserList.length - 3}
+                  </span>
+                </Tooltip>
+              )}
+            </div>
+          </div>
+          <div className={`${clsPrefix}-item-project`}>
+            {!orgFlag && (
+              <div className={`${clsPrefix}-item-project-logo`}>
+                <Tooltip placement="top" title={`所属项目：${projectName}`}><div style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : getRandomBackground(organizationId || projectId + 1) }}>{imageUrl ? '' : String(projectName || organizationName)[0].toUpperCase()}</div></Tooltip>
+              </div>
             )}
+            <Tooltip placement="top" title={knowledgeBaseName}><span className={`${clsPrefix}-item-project-text ${!orgFlag ? `${clsPrefix}-item-project-text-dot` : ''}`}>{knowledgeBaseName}</span></Tooltip>
+            {orgFlag && <span className="c7n-workbench-doc-item-org">组织</span>}
+          </div>
+          <div className="c7n-workbench-doc-item-title">
+            <Tooltip title={title}>
+              <span className="c7n-workbench-doc-item-title-text">{title}</span>
+            </Tooltip>
+            <span className="c7n-workbench-doc-item-title-time">
+              <TimePopover datetime={lastUpdateDate} />
+            </span>
           </div>
         </div>
-        <div className={`${clsPrefix}-item-project`}>
-          {!orgFlag && (
-            <div className={`${clsPrefix}-item-project-logo`}>
-              <Tooltip placement="top" title={`所属项目：${projectName}`}><div style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : getRandomBackground(organizationId || projectId + 1) }}>{imageUrl ? '' : String(projectName || organizationName)[0].toUpperCase()}</div></Tooltip>
-            </div>
-          )}
-          <Tooltip placement="top" title={knowledgeBaseName}><span className={`${clsPrefix}-item-project-text ${!orgFlag ? `${clsPrefix}-item-project-text-dot` : ''}`}>{knowledgeBaseName}</span></Tooltip>
-          {orgFlag && <span className="c7n-workbench-doc-item-org">组织</span>}
-        </div>
-        <div className="c7n-workbench-doc-item-title">
-          <Tooltip title={title}>
-            <span className="c7n-workbench-doc-item-title-text">{title}</span>
-          </Tooltip>
-          <span className="c7n-workbench-doc-item-title-time">
-            <TimePopover datetime={lastUpdateDate} />
-          </span>
-        </div>
-      </div>
-    ));
+      );
+    });
   }
   const loadMore = async () => {
     // await docStore.axiosGetDoc(self);
-    await docDs.nextPage().then(res => {
+    await docDs.nextPage().then((res) => {
       docStore.setDocData(docStore.getDocData.concat(res.toData().list));
     });
   };
@@ -172,7 +190,8 @@ const Doc = ({ history }) => {
                   endMessage={(
                     <span style={{ height: docStore.getDocData.length < 5 ? '1.32rem' : 'auto' }} className={`${clsPrefix}-scroll-bottom`}>{docStore.getDocData.length && docDs.totalPage > 1 ? '到底了' : ''}</span>
                   )}
-                >{renderItems()}
+                >
+                  {renderItems()}
                 </ScrollContext>
               )
               : (docDs.status !== 'loading' && docDs.currentPage === 1
