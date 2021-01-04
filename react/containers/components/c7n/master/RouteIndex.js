64810cffdaeba8fb90fa7c35114aecfd0af726f5
isaac <273070111@qq.com>
2021-01-04 14:33:01 +0800

    [FIX]修改主题问题

--- react/containers/components/c7n/master/RouteIndex.js
+++ react/containers/components/c7n/master/RouteIndex.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import {
+  Route, Switch, withRouter, Redirect, BrowserRouter
+} from 'react-router-dom';
 import { ModalContainer } from 'choerodon-ui/pro/lib';
-import asyncRouter from '../util/asyncRouter';
 import { inject } from 'mobx-react';
+import asyncRouter from '../util/asyncRouter';
 
 const Projects = asyncRouter(() => import('../routes/projects'));
 const Applications = asyncRouter(() => import('../routes/applications'));
@@ -11,30 +13,32 @@ const Unauthorized = asyncRouter(() => import('../routes/unauthorized'));
 const WorkBench = asyncRouter(() => import('../routes/workBench'));
 const ProjectsPro = asyncRouter(() => import('../routes/projectsPro'));
 const ProjectOverview = asyncRouter(() => import('../routes/projectOverview'));
-const InnerIndex = ({ match, AutoRouter, AppState }) => {
-  return  (
-    <div>
-      <Switch>
-        <Route exact path={`${match.url}projects`} component={ProjectsPro} />
-        <Route exact path={`${match.url}applications`} component={Applications} />
-        <Route exact path={`${match.url}charts`} component={Charts} />
-        <Route exact path={`${match.url}unauthorized`} component={Unauthorized} />
-        <Route exact path={`${match.url}workbench`} component={() => {
+const InnerIndex = ({ match, AutoRouter, AppState }) => (
+  <div>
+    <Switch>
+      <Route exact path={`${match.url}projects`} component={ProjectsPro} />
+      <Route exact path={`${match.url}applications`} component={Applications} />
+      <Route exact path={`${match.url}charts`} component={Charts} />
+      <Route exact path={`${match.url}unauthorized`} component={Unauthorized} />
+      <Route
+        exact
+        path={`${match.url}workbench`}
+        component={() => {
           if (AppState.currentMenuType.organizationId) {
-            return <WorkBench />
+            return <WorkBench />;
           }
           return '';
-        }} />
-        <Route exact path={`${match.url}projectsPro`} component={ProjectsPro} />
-        <Route exact path={`${match.url}agile/project-overview`} component={ProjectOverview} />
-        <Route exact path="/">
-          <Redirect to={`${match.url}workbench`} />
-        </Route>
-        <Route path={match.url} component={AutoRouter} />
-      </Switch>
-      <ModalContainer />
-    </div>
-  )
-};
+        }}
+      />
+      <Route exact path={`${match.url}projectsPro`} component={ProjectsPro} />
+      <Route exact path={`${match.url}agile/project-overview`} component={ProjectOverview} />
+      <Route exact path="/">
+        <Redirect to={`${match.url}workbench`} />
+      </Route>
+      <Route path={match.url} component={AutoRouter} />
+    </Switch>
+    <ModalContainer />
+  </div>
+);
 
 export default withRouter(inject('AppState')(InnerIndex));
