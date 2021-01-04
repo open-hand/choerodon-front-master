928fe9d3f244d7b40cec052ccf1ed3bc0f7174b2
isaac <273070111@qq.com>
2021-01-04 15:52:35 +0800

    [FIX]修改master 样式问题

--- react/containers/components/c7n/routes/projectsPro/components/FormView.js
+++ react/containers/components/c7n/routes/projectsPro/components/FormView.js
@@ -70,7 +70,7 @@ export default function FormView({ context }) {
   return (
     <>
       {renderAvatar()}
-      <Form record={dataSet.current} className="c7n-project-sider">
+      <Form labelLayout="float" record={dataSet.current} className="c7n-project-sider">
         <TextField name="name" />
         <TextField name="code" disabled={dataSet.current.status !== 'add'} />
         <Select
