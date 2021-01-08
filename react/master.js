7f34a7dac87574753f4163f93092aef65705f62c
isaac <273070111@qq.com>
2021-01-08 11:35:10 +0800

    [FIX]修改默认主题

--- react/master.js
+++ react/master.js
@@ -151,7 +151,7 @@ export default class Index extends React.Component {
             <Provider {...stores}>
               <Switch>
                 <Route path="/">
-                  <Container defaultTheme="theme4">
+                  <Container defaultTheme="">
                     <Outward AutoRouter={this.props.AutoRouter} />
                   </Container>
                 </Route>
@@ -176,7 +176,7 @@ export default class Index extends React.Component {
               <Route
                 path="/"
               >
-                <Container defaultTheme="theme4">
+                <Container defaultTheme="">
                   <Master AutoRouter={this.props.AutoRouter} />
                 </Container>
               </Route>
