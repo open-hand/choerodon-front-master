dcdf0f158c5c1fedb93b16bac37915bb43625642
laincarl <1047972853@qq.com>
2021-01-28 11:24:28 +0800

    [ADD]增加react-query

--- react/containers/components/c7n/master/index.js
+++ react/containers/components/c7n/master/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { WEBSOCKET_SERVER } from '@/utils';
 import Master from './Master';
 import PermissionProvider from '../tools/permission/PermissionProvider';
 import WSProvider from '../tools/ws/WSProvider';
 import './index.less';
 
+const queryClient = new QueryClient();
 const PermissionAndWSProviderIndex = ({ AutoRouter }) => (
-  <PermissionProvider>
-    <WSProvider server={WEBSOCKET_SERVER}>
-      <Master AutoRouter={AutoRouter} />
-    </WSProvider>
-  </PermissionProvider>
+  <QueryClientProvider client={queryClient}>
+    <PermissionProvider>
+      <WSProvider server={WEBSOCKET_SERVER}>
+        <Master AutoRouter={AutoRouter} />
+      </WSProvider>
+    </PermissionProvider>
+  </QueryClientProvider>
 );
 
 export default PermissionAndWSProviderIndex;
