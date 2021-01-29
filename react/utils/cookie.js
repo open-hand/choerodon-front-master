79159d8a2ded8789cf648dfd9df2ebb0a2d16fd2
laincarl <1047972853@qq.com>
2021-01-29 10:27:05 +0800

    [FIX]回退

--- react/utils/cookie.js
+++ react/utils/cookie.js
@@ -2,17 +2,11 @@ import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
-const setCookie = (name, value, option) => {
-  cookies.set(name, value, option);
-  localStorage.setItem(name, value);
-};
+const setCookie = (name, value, option) => cookies.set(name, value, option);
 
-const getCookie = (name, option) => localStorage.getItem(name) || cookies.get(name, option);
+const getCookie = (name, option) => cookies.get(name, option);
 
-const removeCookie = (name, option) => {
-  cookies.remove(name, option);
-  localStorage.removeItem(name);
-};
+const removeCookie = (name, option) => cookies.remove(name, option);
 
 export {
   setCookie,
