d0a69a7a833d999be2109ffb91186007b3b2ae79
DingZhicheng <zhicheng.ding@hand-china.com>
2021-03-08 19:52:24 +0800

    [IMP]更改自动刷新捕获error判断

--- react/utils/autoRefresh.js
+++ react/utils/autoRefresh.js
@@ -5,7 +5,9 @@ function autoRefresh() {
 }
 
 function register() {
+  console.log('register autoRefresh.');
   window.addEventListener('error', (e) => {
+    console.log('listen a error', e);
     if ((findJs(e) || findCss(e)) && timeLimit()) {
       localStorage.setItem('refresh.latest.time', moment().format('YYYY-MM-DD HH:mm:ss'));
       window.location.reload();
@@ -22,7 +24,7 @@ function findCss(e) {
 function timeLimit() {
   const latestTime = localStorage.getItem('refresh.latest.time');
   const isNeedRefresh = !latestTime || !moment(latestTime, 'YYYY-MM-DD HH:mm:ss').isValid()
-        || moment(latestTime, 'YYYY-MM-DD HH:mm:ss').add('1', 'minute').diff(moment()) < 0;
+    || moment(latestTime, 'YYYY-MM-DD HH:mm:ss').add('1', 'minute').diff(moment()) < 0;
   return isNeedRefresh;
 }
 export default autoRefresh;
