655234dcdaffccc788ebe586b29c9b85ae9ce331
林岩芳 <704565704@qq.com>
2021-04-06 20:54:23 +0800

    Update initUiConfig.js

--- react/common/initUiConfig.js
+++ react/common/initUiConfig.js
@@ -20,6 +20,7 @@ const UI_CONFIG = {
   tableColumnResizable: false,
   modalOkFirst: false,
   modalKeyboard: false,
+  drawerSectionBorder: false,
   drawerOkFirst: true,
   buttonFuncType: 'flat',
   lovQueryUrl: (code) => `/iam/choerodon/v1/lov/code?code=${code}`,
