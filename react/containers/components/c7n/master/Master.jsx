5a29726dd3ef352d57741c38f40c5d26724e2a60
laincarl <1047972853@qq.com>
2021-04-23 09:45:38 +0800

    [FIX]修复新主题表格样式

--- react/containers/components/c7n/master/Master.jsx
+++ react/containers/components/c7n/master/Master.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { initUiConfigure } from '@/common/initUiConfig';
 import { withRouter } from 'react-router';
+import useTheme from '@/hooks/useTheme';
 import MasterDefault from './MasterDefault';
 
 const InitUiConfigMaster = ({ AutoRouter, location }) => {
+  const [theme] = useTheme();
   useEffect(() => {
-    initUiConfigure();
-  }, []);
+    initUiConfigure(theme);
+  }, [theme]);
 
   return (
     <MasterDefault
