dfb6f934b7b543f672aee4958f1ddf4f9f87bbfd
isaac <273070111@qq.com>
2021-06-17 11:28:00 +0800

    [ADD]增加tabcode

--- react/utils/tabCode.ts
+++ react/utils/tabCode.ts
@@ -3,6 +3,10 @@ const TabCodeMap = new Map([
     name: '用例库',
     tabCodes: ['test-case', 'api-test-case'],
   }],
+  ['/devops/code-management', {
+    name: '代码管理',
+    tabCodes: ['branch', 'merge_request', 'pipeline', 'tag', 'code-quality'],
+  }]
 ]);
 
 export default TabCodeMap;
