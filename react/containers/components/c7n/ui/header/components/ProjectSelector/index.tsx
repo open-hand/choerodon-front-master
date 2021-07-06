5fe0857f46ab1b6da829effff515ebcb7501b4a2
王坤奇 <kunqi.wang@hand-china.com>
2021-07-06 13:38:48 +0800

    Revert "[FIX]左上角切换项目时，先跳转再设置menuType，避免页面拿到错的值"
    This reverts commit f253e17abd5ad441cd102d788d15138042d8ffae

--- react/containers/components/c7n/ui/header/components/ProjectSelector/index.tsx
+++ react/containers/components/c7n/ui/header/components/ProjectSelector/index.tsx
@@ -9,12 +9,12 @@ import { useDebounce } from 'ahooks';
 import queryString from 'query-string';
 import findFirstLeafMenu from '@/containers/components/util/findFirstLeafMenu';
 import { historyPushMenu } from '@/utils';
-import MenuStore, { getMenuType } from '@/containers/stores/c7n/MenuStore';
+import MenuStore from '@/containers/stores/c7n/MenuStore';
 import getSearchString from '@/containers/components/c7n/util/gotoSome';
 import './index.less';
 import { withRouter } from 'react-router';
 
-const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props: any) => {
+const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props:any) => {
   const Ref = React.createRef<any>();
 
   const prefixCls = 'c7ncd-dropDownPro';
@@ -29,15 +29,15 @@ const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props: any) 
     HeaderStore,
   } = props;
 
-  function handleClickProject(data: any) {
+  function handleClickProject(data:any) {
     const {
       id, name, organizationId, category,
     } = data;
 
     const type = 'project';
     HeaderStore.setRecentItem(data);
-    // @ts-ignore
-    MenuStore.loadMenuData({ type, id }, false, false).then((menus) => {
+
+    MenuStore.loadMenuData({ type, id }, false).then((menus) => {
       let route = '';
       let path;
       let domain;
@@ -54,29 +54,21 @@ const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props: any) 
       }
       if (path) {
         historyPushMenu(history, path, domain);
-        // @ts-ignore
-        const t = getMenuType({ type, id }, false) || 'site';
-        if (t !== 'user') {
-          AppState.currentMenuType.type = t;
-          if (id) {
-            AppState.currentMenuType.id = id;
-          }
-        }
       }
       AppState.getProjects();
     });
   }
 
-  const handleClickPopContent = (value: any) => {
+  const handleClickPopContent = (value:any) => {
     setProjectFilter('');
     if (value.includes('star')) {
-      const starItem = AppState.getStarProject.find((i: any) => String(i.id) === String(value.split('_')[1]));
+      const starItem = AppState.getStarProject.find((i:any) => String(i.id) === String(value.split('_')[1]));
       AppState.setDropDownPro(`项目: ${starItem.name}`);
       handleClickProject(starItem);
       Ref.current.setPopup(false);
       Ref.current.text = `项目: ${starItem.name}`;
     } else {
-      const recentItem = AppState.getRecentUse.find((i: any) => String(i.id) === String(value.split('_')[1]));
+      const recentItem = AppState.getRecentUse.find((i:any) => String(i.id) === String(value.split('_')[1]));
 
       AppState.setDropDownPro(`项目: ${recentItem.name}`);
       handleClickProject(recentItem);
@@ -86,7 +78,7 @@ const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props: any) 
     }
   };
 
-  async function goto(obj: any) {
+  async function goto(obj:any) {
     const queryObj = queryString.parse(history.location.search);
     const search = await getSearchString('organization', 'id', queryObj.organizationId);
     MenuStore.setActiveMenu(null);
@@ -102,26 +94,26 @@ const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props: any) 
   };
 
   const getPopContent = () => {
-    const filterStarProject = AppState.getStarProject.filter((i: any) => i.name.includes(debouncedFilter));
-    const filterRecentUser = AppState.getRecentUse.filter((i: any) => i.name.includes(debouncedFilter));
+    const filterStarProject = AppState.getStarProject.filter((i:any) => i.name.includes(debouncedFilter));
+    const filterRecentUser = AppState.getRecentUse.filter((i:any) => i.name.includes(debouncedFilter));
     return (
       <div className={`${prefixCls}-popContent`}>
         <p className={`${prefixCls}-popContent-label`}>星标项目</p>
         {
-          filterStarProject && filterStarProject.length > 0 ? filterStarProject.map((i: any) => (
-            <p role="none" onClick={() => handleClickPopContent(`star_${i.id}`)} className={`${prefixCls}-popContent-option`}>{i.name}</p>
-          )) : (
-            <p className={`${prefixCls}-popContent-noOption`}>暂无星标项目</p>
-          )
-        }
+            filterStarProject && filterStarProject.length > 0 ? filterStarProject.map((i:any) => (
+              <p role="none" onClick={() => handleClickPopContent(`star_${i.id}`)} className={`${prefixCls}-popContent-option`}>{i.name}</p>
+            )) : (
+              <p className={`${prefixCls}-popContent-noOption`}>暂无星标项目</p>
+            )
+          }
         <p className={`${prefixCls}-popContent-label`}>最近使用</p>
         {
-          filterRecentUser && filterRecentUser.length > 0 ? filterRecentUser.map((i: any) => (
-            <p role="none" onClick={() => handleClickPopContent(`recent_${i.id}`)} className={`${prefixCls}-popContent-option`}>{i.name}</p>
-          )) : (
-            <p className={`${prefixCls}-popContent-noOption`}>暂无最近使用项目</p>
-          )
-        }
+            filterRecentUser && filterRecentUser.length > 0 ? filterRecentUser.map((i:any) => (
+              <p role="none" onClick={() => handleClickPopContent(`recent_${i.id}`)} className={`${prefixCls}-popContent-option`}>{i.name}</p>
+            )) : (
+              <p className={`${prefixCls}-popContent-noOption`}>暂无最近使用项目</p>
+            )
+          }
         <p role="none" onClick={handleGoAllProject} className={`${prefixCls}-popContent-allPro`}>
           <div>
             <Icon type="multistage_combo_box" />
@@ -140,7 +132,7 @@ const ProjectSelector = inject('AppState', 'HeaderStore')(observer((props: any) 
       ref={Ref as any}
       searchable
       value={AppState.getDropDownPro}
-      onInput={(e: FormEvent<any>) => {
+      onInput={(e:FormEvent<any>) => {
         // @ts-expect-error
         setProjectFilter(e.target.value);
       }}
