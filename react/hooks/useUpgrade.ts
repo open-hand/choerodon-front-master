bd4d6eb413111e709d3162828cf555504b5ce988
20615 <wenfei.li@hand-china.com>
2021-07-07 13:46:56 +0800

    [IMP] 更改useGrade

--- react/hooks/useUpgrade.ts
+++ react/hooks/useUpgrade.ts
@@ -1,34 +1,23 @@
-// @ts-nocheck
 import { useQuery, UseQueryOptions } from 'react-query';
+import { get as getInject } from '@choerodon/inject';
 import useOrganizationKey from './useOrganizationKey';
-import axios from '../containers/components/c7n/tools/axios';
 
 export interface UpgradeConfig {
   organizationId: string
 }
 
+const checkUpgrade = (organizationId: string) => {
+  if (getInject('base-pro:checkUpgrade')) {
+    return getInject('base-pro:checkUpgrade')(organizationId);
+  }
+  return false;
+};
+
 export default function useUpgrade(config: UpgradeConfig, options?: UseQueryOptions<{ failed: boolean } | boolean>) {
   const key = useOrganizationKey({ key: ['upgrade'], organizationId: config.organizationId });
-  return useQuery(key, () => axios({
-    url: '/iam/choerodon/v1/register_saas/check_upgrade',
-    method: 'get',
-    params: { tenantId: config.organizationId },
-  }), {
+  return useQuery(key, () => checkUpgrade(config.organizationId), {
     enabled: !!config.organizationId,
     initialData: false,
-    select: (res) => {
-      try {
-        if (res && res.failed) {
-          return false;
-        }
-        if (res.status === 204) {
-          return false;
-        }
-        return !!res;
-      } catch (e) {
-        return false;
-      }
-    },
     ...options,
   });
 }
