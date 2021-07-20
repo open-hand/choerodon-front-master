248f070523d0aec6760f0724acbcc01d08798185
isaac <273070111@qq.com>
2021-07-20 14:59:38 +0800

    [FIX]删除userCheck代码

--- react/containers/components/c7n/master/MasterDefault.jsx
+++ react/containers/components/c7n/master/MasterDefault.jsx
@@ -47,10 +47,6 @@ const routeWithNoMenu = [{
   code: 'app-market',
 }];
 
-const { Column } = Table;
-
-let maxLength = 0;
-
 function parseQueryToMenuType(search) {
   const menuType = {};
   if (search) {
@@ -86,54 +82,6 @@ function parseQueryToMenuType(search) {
   return menuType;
 }
 
-const HAS_BASE_PRO = C7NHasModule('@choerodon/base-pro');
-
-let ExceedCountUserDataSet;
-
-const OwnerTitle = liteObserver((props) => {
-  const { ds } = props;
-
-  const [num, setNum] = useState(0);
-
-  useEffect(() => {
-    setNum(ds.selected.length);
-  }, [ds.selected]);
-
-  return (
-    <p className="c7ncd-master-header">
-      <span>选择组织用户</span>
-      <span>
-        (已选择
-        <span>{num || 0}</span>
-        人)
-      </span>
-    </p>
-  );
-});
-
-const OwnerModal = liteObserver((props) => {
-  const {
-    num,
-    ds,
-  } = props;
-
-  return (
-    <div className="c7ncd-master-owner">
-      <p>
-        <Icon type="info" />
-        {`因您购买的高级版套餐最多允许组织内${num}人同时使用，请在组织下已有用户中选择${num}人。未选中的用户后续将不能进入该组织。`}
-      </p>
-      <Table
-        dataSet={ds}
-      >
-        <Column name="realName" />
-        <Column name="email" />
-        <Column name="roleNames" />
-      </Table>
-    </div>
-  );
-});
-
 @withRouter
 @inject('AppState', 'MenuStore', 'HeaderStore')
 @observer
@@ -141,6 +89,7 @@ class Masters extends Component {
   constructor(props) {
     super(props);
     this.cRef = createRef();
+    this.userRef = createRef();
   }
 
   componentWillMount() {
@@ -192,151 +141,16 @@ class Masters extends Component {
     const newParams = new URLSearchParams(newProps.location.search);
     const oldParams = new URLSearchParams(oldProps.location.search);
     if (newParams.get('organizationId') !== oldParams.get('organizationId')) {
-      this.getUserCountCheck(newParams.get('organizationId'));
+      if (this.userRef?.current?.getUserCountCheck) {
+        this.userRef.current.getUserCountCheck(newParams.get('organizationId'))
+      }
+      // this.getUserCountCheck(newParams.get('organizationId'));
       // this.props.AppState.getProjects();
     }
   }
 
   componentDidMount() {
     this.initFavicon();
-
-    this.getUserCountCheck();
-
-    ExceedCountUserDataSet = new DataSet({
-      autoQuery: false,
-      cacheSelection: true,
-      primaryKey: 'id',
-      transport: {
-        read: ({ data }) => {
-          const { orgId } = data;
-          return ({
-            url: MasterApis.getPageMemberUrl(orgId),
-            method: 'get',
-            transformResponse: (res) => {
-              let newRes = res;
-              try {
-                newRes = JSON.parse(res);
-                newRes.content = newRes.content.map((i) => {
-                  i.roleNames = i.roleNames.join(',');
-                  return i;
-                });
-                return newRes;
-              } catch (e) {
-                return newRes;
-              }
-            },
-          });
-        },
-      },
-      fields: [{
-        name: 'realName',
-        type: 'string',
-        label: '用户名',
-      }, {
-        name: 'email',
-        type: 'string',
-        label: '邮箱',
-      }, {
-        name: 'roleNames',
-        type: 'string',
-        label: '组织角色',
-      }],
-      events: {
-        select: ({ dataSet, record }) => {
-          this.setRecordByMaxLength(dataSet, record, maxLength, true);
-        },
-        unSelect: ({ dataSet, record }) => {
-          this.setRecordByMaxLength(dataSet, record, maxLength, false);
-        },
-        load: ({ dataSet }) => {
-          dataSet.records.forEach((i) => {
-            if (i.get('owner')) {
-              i.selectable = false;
-              i.isSelected = true;
-            }
-          });
-        },
-      },
-    });
-    // if (pathname.includes('access_token') && pathname.includes('token_type') && localStorage.getItem(`historyPath-${getUserId}`)) {
-    //   window.location = `/#${localStorage.getItem(`historyPath-${getUserId}`)}`;
-    // }
-  }
-
-	setRecordByMaxLength = (ds, re, length, selectIf) => {
-	  const selectedLength = ds.selected.length;
-	  const selectedIds = ds.selected.map((i) => i.id);
-	  if (selectIf) {
-	    if (selectedLength >= length) {
-	      ds.records.forEach((i) => {
-	        if (!selectedIds.includes(i.id)) {
-	          i.selectable = false;
-	        }
-	      });
-	    }
-	  } else {
-	    ds.records.forEach((i) => {
-	      if (!i.get('owner')) {
-	        i.selectable = true;
-	      }
-	    });
-	  }
-	}
-
-  getUserCountCheck = async (orgId) => {
-    const organizationId = orgId || this.props.AppState.currentMenuType.organizationId;
-    if (HAS_BASE_PRO) {
-      if (organizationId) {
-        const res = await MasterServices.axiosGetCheckUserCount(organizationId);
-        if (res && !res.data && res.data !== '') {
-          // 用户超过套餐任务
-          maxLength = res;
-          const user = await MasterServices.axiosGetCheckOwner(organizationId);
-          if (user && user.data === '') {
-            // 当前用户就是注册者
-            ExceedCountUserDataSet.setQueryParameter('orgId', organizationId);
-            await ExceedCountUserDataSet.query();
-            Modal.open({
-              maskClosable: false,
-              style: {
-                width: 820,
-              },
-              okCancel: false,
-              key: Modal.key(),
-              title: <OwnerTitle ds={ExceedCountUserDataSet} />,
-              children: <OwnerModal num={res} ds={ExceedCountUserDataSet} />,
-              onOk: async () => {
-                const selectedLength = ExceedCountUserDataSet.selected.length;
-                if (selectedLength > maxLength) {
-                  message.error(`请选择${maxLength}个用户`);
-                  return false;
-                }
-                try {
-                  await MasterServices.axiosDeleteCleanMember(organizationId, ExceedCountUserDataSet.selected.map((i) => i.get('id')));
-                  this.getUserCountCheck();
-                } catch (e) {
-                  return false;
-                }
-              },
-            });
-          } else {
-            // 此user是注册者
-            const { email, realName } = user;
-            Modal.open({
-              maskClosable: false,
-              key: Modal.key(),
-              title: 'SaaS组织升级中',
-              children: `您所在组织的组织所有者${realName}(${email})升级组织后尚未确认组织用户，请联系组织所有者确认。`,
-              footer: null,
-            });
-          }
-        }
-        return true;
-      }
-      setTimeout(() => {
-        this.getUserCountCheck();
-      }, 500);
-    }
   }
 
   updateTheme = (newPrimaryColor) => {
@@ -617,6 +431,14 @@ class Masters extends Component {
                 popoverHead,
                 cRef: this.cRef,
               })}
+              {
+                mount('base-pro:UserCheck', {
+                  ...this.props,
+                  MasterServices,
+                  MasterApis,
+                  cRef: this.userRef,
+                })
+              }
               <div id="autoRouter" className="content">
                 {
                   AppState.getCanShowRoute || defaultBlackList.some((v) => this.props.location.pathname.startsWith(v)) ? (
