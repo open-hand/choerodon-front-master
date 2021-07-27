64a0f0656524ccaa5a90312002ee15d0a44cd6de
20399 <yanfang.lin@hand-china.com>
2021-07-27 11:23:00 +0800

    [IMP] 优化登录失效页面样式

--- react/containers/components/c7n/routes/unauthorized/index.js
+++ react/containers/components/c7n/routes/unauthorized/index.js
@@ -3,14 +3,13 @@ import { Button } from 'choerodon-ui/pro';
 import { authorizeC7n as authorize } from '@/utils';
 import './index.less';
 
-
 function ListView() {
   return (
     <div className="c7n-master-unauthorized">
       <div className="c7n-master-unauthorized-content">
         <div className="c7n-master-unauthorized-picture" />
         <div className="c7n-master-unauthorized-text">抱歉，您未登录或身份认证已失效，请点击下方按钮重新登录！</div>
-        <Button onClick={authorize} className="relogin" funcType="raised" color="blue">
+        <Button onClick={authorize} className="relogin" funcType="raised" color="primary">
           重新登录
         </Button>
       </div>
