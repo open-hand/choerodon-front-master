955afab7f4c33bc58cece9c17cb1af649a949859
hulingfangzi <lingfangzi.hu01@hand-china.com>
2021-08-04 17:19:28 +0800

    [FIX] 修复创建自定义视图偶发报错

--- react/containers/components/c7n/routes/workBench/list/view/components/CreateViewDrawer/index.js
+++ react/containers/components/c7n/routes/workBench/list/view/components/CreateViewDrawer/index.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import { StoreProvider } from './stores';
-import CreateViewDrawer from './CreateViewDrawer';
 
-const index = (props) => (
-  <StoreProvider {...props}>
-    <CreateViewDrawer />
-  </StoreProvider>
-);
+// 创建布局视图侧滑
+import React, { useMemo, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import queryString from 'query-string';
+import {
+  DataSet, Form, SelectBox, TextField, Select,
+} from 'choerodon-ui/pro';
+import { isEmpty } from 'lodash';
+import { observer } from 'mobx-react-lite';
+// import { useWorkBenchStore } from '../../../../stores'; // TODO 优化
+// import Store from './stores';
+import CreateViewDataSet from './stores/CreateViewDataSet';
 
-export default index;
+const CreateViewDrawer = observer(({ modal, search, viewDs, onCreateCustomView }) => {
+  const history = useHistory();
+  const createViewDS = useMemo(() => new DataSet(CreateViewDataSet()), []);
+
+  useEffect(() => {
+    createViewDS.create();
+  }, []);
+
+  modal.handleOk(async () => {
+    const view = createViewDS.current.get('view');
+    const res = await createViewDS.submit();
+
+    if (!isEmpty(res) && res.success) {
+      const { dashboardId, dashboardName } = res.list[0];
+      if (view === 'INTERNAL') {
+        await viewDs.query();
+        viewDs.locate(viewDs.findIndex((r) => r.get('dashboardId') === dashboardId));
+        onCreateCustomView(dashboardId);
+      } else {
+        // 如果创建的是自定义视图 跳转到自定义视图
+        let searchParams = queryString.parse(search);
+        searchParams = { ...searchParams, dashboardId, dashboardName };
+        history.push({
+          pathname: '/workbench/edit',
+          search: `?${queryString.stringify(searchParams)}`,
+        });
+      }
+      // relationDS.query();
+      return true;
+    }
+    if (res === undefined) {
+      return true;
+    }
+    // 提交后后端返回报错信息
+    return false;
+  });
+
+  return (
+    <Form dataSet={createViewDS}>
+      <SelectBox name="view" />
+      {createViewDS.current?.get('view') === 'INTERNAL' && <Select name="dashboardId" noCache />}
+      {createViewDS.current?.get('view') !== 'INTERNAL' && <TextField name="dashboardName" />}
+    </Form>
+  );
+});
+
+export default CreateViewDrawer;
