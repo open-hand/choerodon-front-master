93c4bd41a11f0aedcfcbf57f1ba4b31ecf1e465b
isaac <273070111@qq.com>
2021-10-08 16:16:53 +0800

    [FIX]修改菜单展开收起逻辑

--- react/containers/components/c7n/ui/menu/index.js
+++ react/containers/components/c7n/ui/menu/index.js
@@ -425,7 +425,7 @@ export default class CommonMenu extends Component {
             mode="inline"
             inlineCollapsed={collapsed}
             selectedKeys={[activeMenu && activeMenu.code]}
-            openKeys={openKeys.slice()}
+            openKeys={collapsed ? [] : openKeys.slice()}
             onOpenChange={this.handleOpenChange}
             subMenuCloseDelay={0.1}
             subMenuOpenDelay={0.1}
