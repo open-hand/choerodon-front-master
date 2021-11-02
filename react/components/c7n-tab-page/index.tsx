a86fbe759492a146cf135a3577c16bb35e520e30
翁恺敏 <736653759@qq.com>
2021-11-03 00:43:24 +0800

    [IMP] rebuild the tabPage PageTab PageTabWrapper

--- react/components/c7n-tab-page/index.tsx
+++ react/components/c7n-tab-page/index.tsx
@@ -1,4 +1,4 @@
 export { default as PageTab } from './components/PageTab';
 export { default as TabPage } from './components/TabPage';
-export { default as PageWrapper } from './components/PageWrapper';
+export { default as PageWrap } from './components/PageWrapper';
 export * from './components/PageWrapper';
