4ed88f1765c90d4247e3bff4ade59a7aa91d388d
翁恺敏 <736653759@qq.com>
2021-11-28 08:43:22 +0800

    [FIX] fix typings

--- react/typings/react-intl-types.ts
+++
@@ -1,7 +0,0 @@
-import { MessageDescriptor } from 'react-intl';
-
-export type MessageFormatPrimitiveValue = string | number | boolean | null | undefined
-
-export type FormatterValues = Record<string, MessageFormatPrimitiveValue | React.ReactElement | any>
-
-export type FormatFunctionTypes = (props:MessageDescriptor, values?:FormatterValues)=> string | React.ReactNode
