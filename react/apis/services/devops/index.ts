16271db48e0e518073dcebfa2beba92e98ec7a86
翁恺敏 <736653759@qq.com>
2021-11-30 10:13:54 +0800

    [ADD] add api and fix locale

--- react/apis/services/devops/index.ts
+++ react/apis/services/devops/index.ts
@@ -1,2 +1,3 @@
 export * from './pods';
 export * from './cicd-pipeline';
+export * from './app-service';
--- react/locale/zh_CN/header/headerMiddleButtons.js
+++ react/locale/zh_CN/header/headerMiddleButtons.js
@@ -3,7 +3,7 @@ const headerMiddleButtons = {
   'c7ncd.header.middle.lists.item.market': '应用市场',
   'c7ncd.header.middle.lists.item.workCalendar': '工作日历',
   'c7ncd.header.middle.lists.item.knowledge': '知识库',
-  'c7ncd.header.middle.lists.item.workbench': '工作列表',
+  'c7ncd.header.middle.lists.item.workbench': '工作台',
 };
 
 export {
