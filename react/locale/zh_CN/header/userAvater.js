fb42e020d0f351f44e932a4bacef4d8c208fe31a
jamin <245139263@qq.com>
2021-12-07 19:10:06 +0800

    [FIX]-多语言

--- react/locale/zh_CN/header/userAvater.js
+++ react/locale/zh_CN/header/userAvater.js
@@ -2,15 +2,6 @@ const userAvater = {
   'c7ncd.user.avatar.languageSwicth': '切换语言',
   'c7ncd.user.avatar.plateEntry': '平台管理',
   'c7ncd.user.avatar.invitation': '邀请试用',
-  'c7ncd.user.avatar.invitation.link.desc': '将您的专属推广链接分享给好友，邀请好友分享页面进行试用注册，成为推荐达人可领取实物礼品',
-  'c7ncd.user.avatar.invitation.link.link': '邀请链接',
-  'c7ncd.user.avatar.invitation.invitation.desc': '填写好友信息，分享专属邀请给好友，邀请好友在分享页面进行试用注册，成为推荐达人可领实物礼品',
-  'c7ncd.user.avatar.invitation.btnInvitation': '邀请',
-  'c7ncd.user.avatar.invitation.haveInvitationed': '已邀请',
-  'c7ncd.user.avatar.invitation.companyName': '公司名称',
-  'c7ncd.user.avatar.invitation.websiteAddress': '官网地址',
-  'c7ncd.user.avatar.invitation.industry': '行业',
-  'c7ncd.user.avatar.invitation.wants': '想要使用的功能',
   'c7ncd.user.avatar': '',
 };
 
