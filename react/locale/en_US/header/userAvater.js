945c1623eb888185955bbc89c263d859551855cb
jamin <245139263@qq.com>
2021-12-08 09:57:44 +0800

    [FIX]-注册邀请多语言

--- react/locale/en_US/header/userAvater.js
+++ react/locale/en_US/header/userAvater.js
@@ -2,6 +2,16 @@ const userAvater = {
   'c7ncd.user.avatar.languageSwicth': 'Swicth Language',
   'c7ncd.user.avatar.plateEntry': 'Platform Management',
   'c7ncd.user.avatar.invitation': 'Invitation',
+  'c7ncd.user.avatar.invitation.link.desc': 'Share your exclusive promotion link with your friends, invite friends to share the page for trial registration, and become the recommender to receive physical gifts',
+  'c7ncd.user.avatar.invitation.link.link': 'Invite Link',
+  'c7ncd.user.avatar.invitation.invitation.desc': "Fill in your friend's information, share your exclusive invitation to your friends, invite your friends to register for trial on the sharing page, and become a recommended person to receive physical gifts",
+  'c7ncd.user.avatar.invitation.btnInvitation': 'Invitation',
+  'c7ncd.user.avatar.invitation.haveInvitationed': 'Has invited',
+  'c7ncd.user.avatar.invitation.companyName': 'CompanyName',
+  'c7ncd.user.avatar.invitation.websiteAddress': 'WebsiteAddress',
+  'c7ncd.user.avatar.invitation.industry': 'Industry',
+  'c7ncd.user.avatar.invitation.wants': 'Wants',
+  'c7ncd.user.avatar': '',
 };
 
 export {
