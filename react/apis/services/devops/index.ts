5db806cf26b910eab0d2086c796f53611fcdc300
isaac <273070111@qq.com>
2021-12-28 09:36:27 +0800

    [FIX]修改apis

--- react/apis/services/devops/index.ts
+++ react/apis/services/devops/index.ts
@@ -5,3 +5,4 @@ export * from './Organizations';
 export * from './template_steps';
 export * from './pipelineTemplate';
 export * from './ci-template-job-group';
+export * from './ci-template-step-category';
