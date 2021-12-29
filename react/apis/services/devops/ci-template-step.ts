f717ca08b063443971bdad0fa6cea29c8531b794
isaac <273070111@qq.com>
2021-12-29 15:13:48 +0800

    [ADD]增加apis

--- react/apis/services/devops/ci-template-step.ts
+++ react/apis/services/devops/ci-template-step.ts
@@ -21,14 +21,14 @@ class CiTemplateStepApi extends Api<CiTemplateStepApi> {
 
   getSiteSteps() {
     return this.request({
-      url: `${this.sitePrefix}`,
+      url: `${this.sitePrefix}/list`,
       method: 'get',
     });
   }
 
   getOrgSteps() {
     return this.request({
-      url: `${this.orgPrefix}`,
+      url: `${this.orgPrefix}/list`,
       method: 'get',
     });
   }
