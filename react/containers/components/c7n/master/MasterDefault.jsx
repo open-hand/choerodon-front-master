5632091247a699f6aa33e2d32b6250eb3bf7f22c
翁恺敏 <736653759@qq.com>
2021-12-31 14:41:06 +0800

    [FIX] fix data

--- react/containers/components/c7n/master/MasterDefault.jsx
+++ react/containers/components/c7n/master/MasterDefault.jsx
@@ -156,10 +156,7 @@ class Masters extends Component {
 
   setDocUrl = async (params) => {
     if (JSON.stringify(params) !== '{}') {
-      // const result = await MasterServices.axiosGetHelpDoc(params);
-      // if (result) {
       this.props.AppState.setDocUrl('https://open.hand-china.com/document-center/doc/product/10177/10608?doc_id=168204&doc_code=118818');
-      // }
     }
   };
 
