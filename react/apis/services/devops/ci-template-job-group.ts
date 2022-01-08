d6f1c3c753b04901aebfe27291d53d39449de910
isaac <273070111@qq.com>
2022-01-08 18:23:54 +0800

    [FIX]修改问题

--- react/apis/services/devops/ci-template-job-group.ts
+++ react/apis/services/devops/ci-template-job-group.ts
@@ -5,9 +5,20 @@ class CiTemplateJobGroupApi extends Api<CiTemplateJobGroupApi> {
     return `/devops/v1/organizations/${this.orgId}/ci_template_job_group`;
   }
 
+  get sitePrefix() {
+    return '/devops/v1/site/0/ci_template_job_group';
+  }
+
   getList() {
     return this.request({
       url: `${this.orgPrefix}/list`,
+      method: 'get',
+    });
+  }
+
+  getSiteList() {
+    return this.request({
+      url: `${this.sitePrefix}/list`,
       method: 'get',
     });
   }
