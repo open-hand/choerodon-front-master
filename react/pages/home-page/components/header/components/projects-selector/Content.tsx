2eb65105310253e20f18556cb6380dbfb67d6f46
翁恺敏 <736653759@qq.com>
2022-01-13 03:17:15 +0000

    [FIX] fix bugs
    (cherry picked from commit 6d23d7fed2f8b16cfe31f463ec724ed17be1acf4)

--- react/pages/home-page/components/header/components/projects-selector/Content.tsx
+++ react/pages/home-page/components/header/components/projects-selector/Content.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Icon, Select } from 'choerodon-ui/pro';
 import { useHistory } from 'react-router';
+import { Loading } from '@choerodon/components';
 import RecentUseProjectsLists from './components/recent-use-projects-lists';
 import StarProjectsLists from './components/star-projects-lists';
 
@@ -35,8 +36,9 @@ const ProjectsSelector = () => {
   };
 
   const renderSelectorPopupContent = () => (
-    <div className={`${prefixCls}-popup`}>
-      {
+    <Loading display={AppState.isProjectsLoading}>
+      <div className={`${prefixCls}-popup`}>
+        {
         // 通过这个途径获取值
         !selectorRef.current?.text ? (
           <div className={`${prefixCls}-popup-common`}>
@@ -62,7 +64,8 @@ const ProjectsSelector = () => {
           </div>
         )
       }
-    </div>
+      </div>
+    </Loading>
   );
 
   const renderSelectorText = (data: { value: any; text: any; }) => {
