395938a3b7b0a1f296d719e4394c7f44f6f6cf7a
DingZhicheng <zhicheng.ding@hand-china.com>
2022-02-10 15:46:24 +0800

    [IMP]工作台样式敏捷卡片更改

--- react/hooks/useQueryString.ts
+++ react/hooks/useQueryString.ts
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 export default function useQueryString() {
   const location = useLocation();
   const urlParams = new URLSearchParams(location.search);
-  // @ts-expect-error
+  // @ts-ignore
   const params = Object.fromEntries(urlParams);
   return params;
 }
