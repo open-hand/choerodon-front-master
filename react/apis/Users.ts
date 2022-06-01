7634910a8e1938625c202fe7fdfd5076f717d0c4
jamin <245139263@qq.com>
2022-06-01 09:52:43 +0800

    [ADD]-api

--- react/apis/Users.ts
+++ react/apis/Users.ts
@@ -17,17 +17,94 @@ class UsersApi extends Api<UsersApi> {
     });
   }
 
-  /**
+  // 获取第三方应用可以绑定的组织
+
+  openAppGetCanBindOrgs(type:string) {
+    return this.request({
+      method: 'get',
+      url: `${this.prefix}/list_tenants_with_open_app?open_app_type=${type}`,
+    });
+  }
+
+  // 获取第三方应用已经绑定的组织
+  openAppGetBindOrgs(type:string) {
+    return this.request({
+      method: 'get',
+      url: `${this.prefix}/list_organizations_bound_up_with_open_app?open_app_type=${type}`,
+    });
+  }
+
+  yqcloudCheckPhoneExist(phone:string, orgId:string) {
+    return this.request({
+      method: 'get',
+      url: `${this.prefix}/check_yqcloud_email_or_phone_exist?verify_type=PHONE&phone=${phone}&organization_id=${orgId}`,
+    });
+  }
+
+  yqcloudCheckEmailExist(email:string, orgId:string) {
+    return this.request({
+      method: 'get',
+      url: `${this.prefix}/check_yqcloud_email_or_phone_exist?verify_type=EMAIL&email=${email}&organization_id=${orgId}`,
+    });
+  }
+
+  yqcloudBind(data:any) {
+    return this.request({
+      method: 'post',
+      url: `${this.prefix}/bind_yqcloud`,
+      data,
+    });
+  }
+
+  //
+  yqcloudBindStatus(data:any) {
+    return this.request({
+      method: 'get',
+      url: `${this.prefix}/yqcloud_bind_status`,
+      data,
+    });
+  }
+
+  yqcloudUnBind(orgId:string) {
+    return this.request({
+      method: 'post',
+      url: `${this.prefix}/unbind_yqcloud?organization_id=${orgId}`,
+    });
+  }
+
+  // 燕千云绑定获取验证码
+
+   yqcloudBindGetCaptchaByEmail = (email:string, params:any) => this.request({
+     method: 'post',
+     url: `${this.prefix}/get_yqcloud_verify_code`,
+     data: {
+       verifyType: 'EMAIL',
+       email,
+       ...params,
+     },
+   })
+
+   yqcloudBindGetCaptchaByPhone=(phone:string, params:any) => this.request({
+     method: 'post',
+     url: `${this.prefix}/get_yqcloud_verify_code`,
+     data: {
+       verifyType: 'PHONE',
+       phone,
+       ...params,
+     },
+   })
+
+   /**
    *  获取用户个人信息
    * @return {*}
    * @memberof UsersApi
    */
-  getUserInfo() {
-    return this.request({
-      url: `${this.prefix}/self`,
-      method: 'get',
-    });
-  }
+   getUserInfo() {
+     return this.request({
+       url: `${this.prefix}/self`,
+       method: 'get',
+     });
+   }
 }
 
 const usersApi = new UsersApi();
