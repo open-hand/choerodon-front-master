7e3819dc973964f09a67f83a1f8a28c15a89002a
jamin <245139263@qq.com>
2022-06-10 14:36:24 +0800

    [TEST]-PlatformAnnouncement close

--- react/containers/components/c7n/components/PlatformAnnouncement/index.tsx
+++ react/containers/components/c7n/components/PlatformAnnouncement/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button, Icon } from 'choerodon-ui';
 import { Modal } from 'choerodon-ui/pro';
 import get from 'lodash/get';
+import { observer } from 'mobx-react-lite';
 import './index.less';
 import axios from '@/components/axios';
 
@@ -58,11 +59,11 @@ const PlatformAnnouncement = (props:{
     infoModal && infoModal.close();
   };
 
-  const renderAnnounceTitle = () => (
+  const renderAnnounceTitle = useCallback(() => (
     <div className={`${prefixCls}-modal-title`}>
       <span>
         {title}
-        123456
+        qw
       </span>
       <Icon
         style={{
@@ -70,17 +71,13 @@ const PlatformAnnouncement = (props:{
           float: 'right',
         }}
         type={isFull ? 'fullscreen_exit' : 'fullscreen'}
-        onClick={() => setFull(!isFull)}
+        onClick={() => {
+          console.log(isFull);
+          setFull(!isFull);
+        }}
       />
     </div>
-  );
-
-  useEffect(() => {
-    const announcementModalShowed = window.localStorage.getItem('announcementModalInfo')?.split('+')[1] !== 'false';
-    if (!announcementModalShowed) {
-      handleInfo();
-    }
-  }, []);
+  ), [isFull]);
 
   useEffect(() => {
     infoModal && infoModal.update({
@@ -89,6 +86,39 @@ const PlatformAnnouncement = (props:{
     });
   }, [isFull]);
 
+  useEffect(() => {
+    const announcementModalShowed = window.localStorage.getItem('announcementModalInfo')?.split('+')[1] !== 'false';
+    if (!announcementModalShowed) {
+      handleInfo();
+    }
+  }, []);
+
+  const InfoContent = (infoProps:any) => {
+    const { modal } = infoProps;
+
+    const handleOk = () => {
+      window.localStorage.setItem('announcementModalInfo', `${readId}+true`);
+      console.log('gogo');
+      return true;
+    };
+
+    modal.handleOk(handleOk);
+
+    return (
+      <div
+        className="c7n-boot-header-inbox-wrap"
+        style={{
+          maxHeight: '47vh',
+        }}
+      >
+        <div
+          className="c7n-boot-header-inbox-content"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+    );
+  };
+
   const handleInfo = () => {
     infoModal = Modal.open({
       key: infoModalKey,
@@ -96,24 +126,10 @@ const PlatformAnnouncement = (props:{
       style: modalStyle,
       movable: false,
       fullScreen: false,
-      children: (
-        <div
-          className="c7n-boot-header-inbox-wrap"
-          style={{
-            maxHeight: '47vh',
-          }}
-        >
-          <div
-            className="c7n-boot-header-inbox-content"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
-        </div>
-      ),
-      onOk: () => {
-        window.localStorage.setItem('announcementModalInfo', `${readId}+true`);
-        console.log('go');
-        return true;
-      },
+      children: <InfoContent full={isFull} />,
+      // onOk: () => {
+
+      // },
       okText: '关闭',
       okCancel: false,
     });
@@ -130,4 +146,4 @@ const PlatformAnnouncement = (props:{
   );
 };
 
-export default PlatformAnnouncement;
+export default observer(PlatformAnnouncement);
