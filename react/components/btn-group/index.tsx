2576c484c18d7863f84a3f60eeae8b66b5aa6d44
郑皓天 <273070111@qq.com>
2022-06-20 09:40:48 +0800

    [ADD]增加headerbuttons，btnGoupItem中增加icon字段

--- react/components/btn-group/index.tsx
+++ react/components/btn-group/index.tsx
@@ -5,8 +5,8 @@ import React, {
 
 import { Menu, Popover, Icon } from 'choerodon-ui';
 import { Button, Tooltip } from 'choerodon-ui/pro';
-import { flatten, map } from 'lodash';
-import _ from 'lodash';
+import _, { flatten, map } from 'lodash';
+
 import { ButtonColor } from 'choerodon-ui/pro/lib/button/enum';
 import classNames from 'classnames';
 import { Permission } from '@/components/permission';
@@ -57,6 +57,7 @@ const BtnGroup:FC<CustomBtnGroupProps> = (props) => {
         disabled,
         group,
         tooltipsConfig: itemToolTipsConfig,
+        icon: itemIcon,
       } = itemProps;
       const Item = (
         <Menu.Item
@@ -65,6 +66,11 @@ const BtnGroup:FC<CustomBtnGroupProps> = (props) => {
         >
           <Tooltip {...itemToolTipsConfig}>
             <span role="none" onClick={disabled ? () => {} : handler}>
+              {
+                itemIcon ? (
+                  <Icon type={itemIcon} />
+                ) : ''
+              }
               {itemName}
             </span>
           </Tooltip>
