7f0829b3af3ea4a1a1b621ea757e94da49519f39
jamin <245139263@qq.com>
2022-07-05 14:57:44 +0800

    feat(钉钉免登): 钉钉免登外部浏览器跳转

--- react/containers/components/c7n/routes/dingTalkTransition/index.tsx
+++ react/containers/components/c7n/routes/dingTalkTransition/index.tsx
@@ -4,6 +4,7 @@ import * as dd from 'dingtalk-jsapi';
 import Cookies from 'universal-cookie';
 import { useHistory } from 'react-router-dom';
 import useQueryString from '@/hooks/useQueryString';
+import axios from '@/components/axios';
 import { API_HOST } from '@/utils/constants';
 
 const cookies = new Cookies();
@@ -14,44 +15,47 @@ const Index = () => {
   const history = useHistory();
 
   useEffect(() => {
-    if (dd.env.platform === 'notInDingTalk' && corpId) {
-      window.location.href = `${API_HOST}/oauth/open/ding_talk/callback?organization_id=${orgId}&code=${code}&state=STATE&way=web`;
-    } else if (corpId) {
-      if (cookies.get('access_token')) {
-        history.push(sessionStorage.getItem('historyPath') || '/workbench');
-      } else {
-        dd.ready(() => {
-          dd.runtime.permission.requestAuthCode({
-            corpId, // 企业id
-            // @ts-ignore
-            onSuccess(info: any) {
-              const infoCode = info.code; // 通过该免登授权码可以获取用户身份
-              // window.location.href = `${API_HOST}/oauth/open/ding_talk/callback?organization_id=${orgId}&code=${infoCode}&state=STATE&way=web`;
-              dd.biz.util.openLink({
-                url: `${API_HOST}/oauth/open/ding_talk/callback?organization_id=${orgId}&code=${infoCode}&state=STATE&way=web`,
-                // 要打开链接的地址
-                // @ts-ignore
-                onSuccess(result) {
-                  /**/
-                  console.log(result, 'result');
-                },
-                onFail(err:any) {
-                  console.log(err);
-                },
-              });
-              dd.biz.navigation.close({
-                onSuccess(result:any) {
-                  console.log(result, 'closeResult');
-                },
-                onFail(err:any) {
-                  console.log(err, 'closeErr');
-                },
-              });
-            },
+    const dingTalkLogin = async () => {
+      if (dd.env.platform === 'notInDingTalk' && corpId) {
+        window.location.href = `${API_HOST}/oauth/open/ding_talk/callback?organization_id=${orgId}&code=${code}&state=STATE&way=web`;
+      } else if (corpId) {
+        if (cookies.get('access_token')) {
+          history.push(sessionStorage.getItem('historyPath') || '/workbench');
+        } else {
+          const res = await axios.get(`/iam/choerodon/v1/organizations/${orgId}/open_app/internal_browser?app_type=ding_talk`);
+          dd.ready(() => {
+            dd.runtime.permission.requestAuthCode({
+              corpId, // 企业id
+              // @ts-ignore
+              onSuccess(info: any) {
+                const infoCode = info.code; // 通过该免登授权码可以获取用户身份
+                if (res) {
+                  window.location.href = `${API_HOST}/oauth/open/ding_talk/callback?organization_id=${orgId}&code=${infoCode}&state=STATE&way=web`;
+                } else {
+                  dd.biz.util.openLink({
+                    url: `${API_HOST}/oauth/open/ding_talk/callback?organization_id=${orgId}&code=${infoCode}&state=STATE&way=web`,
+                    // @ts-ignore
+                    onSuccess(result) {},
+                    onFail(err:any) {
+                      console.log(err);
+                    },
+                  });
+
+                  dd.biz.navigation.close({
+                    onSuccess(result:any) {},
+                    onFail(err:any) {
+                      console.log(err, 'closeErr');
+                    },
+                  });
+                }
+              },
+            });
           });
-        });
+        }
       }
-    }
+    };
+
+    dingTalkLogin();
   }, []);
 
   return <Loading type="c7n" />;
