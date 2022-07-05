5fbf79964703563dbc1c4c9cb1b676f3560ffe21
isaac <273070111@qq.com>
2022-07-05 17:01:23 +0800

    feat(提单sdk): 修改提单sdk为动态token

--- react/components/yqFeedback/index.tsx
+++ react/components/yqFeedback/index.tsx
@@ -23,34 +23,34 @@ const Index = (props: any): any => {
     const data = await siteApi.getFeedBack();
     if (data.enable) {
       // initSDK();
-      initSecret();
+      initSecret(data?.token);
     }
   };
 
-  const initSecret = async () => {
+  const initSecret = async (token: string) => {
     if (id) {
       const secret = await yqcloudApi.getSDKSecret(id);
       if (secret) {
         sessionStorage.setItem('YQ_USER_SECRET', secret);
-        initSDK();
+        initSDK(token);
       }
     } else if (tryTimes <= 3) {
       tryTimes += 1;
       setTimeout(() => {
-        initSecret();
+        initSecret(token);
       }, 500);
     } else if (tryTimes > 3) {
       message.error('燕千云提单SDK加载失败请重试');
     }
   };
 
-  const initSDK = () => {
+  const initSDK = (token: any) => {
     const flag = document.querySelector('.yq-feedback');
     // eslint-disable-next-line
     if (!flag && window._env_.YQ_FEEDBACK_SDK) {
       const script = document.createElement('script');
       // eslint-disable-next-line
-      script.src = window._env_.YQ_FEEDBACK_SDK;
+      script.src = `${window._env_.YQ_FEEDBACK_SDK}/${token}/feedback.js`;
       script.async = true;
       document.body.appendChild(script);
     }
