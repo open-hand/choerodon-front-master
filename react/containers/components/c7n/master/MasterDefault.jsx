5b81d1676f13b28fa24bdb60bda8a0c3d3aa47c4
isaac <273070111@qq.com>
2022-07-22 09:07:44 +0800

    [FIX]删除多余试用逾期逻辑

--- react/containers/components/c7n/master/MasterDefault.jsx
+++ react/containers/components/c7n/master/MasterDefault.jsx
@@ -223,44 +223,11 @@ class Masters extends Component {
     }
   }
 
-  // 获取SaaS 新用户的免费使用天数提醒
-  getSaaSUserRestDays = async () => {
-    const {
-      organizationId,
-    } = this.props.AppState.currentMenuType || {};
-    if (window._env_.BUSINESS || !organizationId) {
-      return;
-    }
-    try {
-      const res = await getSaaSUserAvilableDays(organizationId);
-      if (res && res.failed) {
-        message.error(res?.message);
-        return;
-      }
-      const { HeaderStore } = this.props;
-
-      const identity = 'saas_restdays_announcement';
-      if (res && (!localStorage.saaslastClosedId || localStorage.saaslastClosedId !== res?.link)) {
-        HeaderStore.innsertAnnouncement(identity, {
-          data: res,
-          onCloseCallback: () => {
-            window.localStorage.setItem('saaslastClosedId', `${res?.link}`);
-          },
-          component: <SaaSUserAnnouncement data={res} />,
-        });
-      }
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
   componentDidMount() {
     this.initFavicon();
 
     // 获取系统公告
     this.getPlatformAnnouncement();
-    // 获取适用天数in the base-pro, only applied in the hand version
-    this.getSaaSUserRestDays();
   }
 
   isInOutward = (pathname) => {
