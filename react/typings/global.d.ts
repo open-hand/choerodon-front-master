e0f1ccbd42d88ff9d1c201d20efe6bde673518ef
丁志诚 <zhicheng.ding@zknow.com>
2022-10-09 19:34:04 +0800

    feat: windows全局变量添加history保存&移除过期组件&全局ts类型定义更换位置

---
+++ react/typings/global.d.ts
@@ -0,0 +1,19 @@
+import type { History } from 'history';
+
+declare global {
+    interface Window {
+        /** 内部的to 跳转方法使用的history */
+        ___choeordonHistory__: History | null
+        _env_: {
+            localRouteName: any
+            YQ_FEEDBACK_SDK: string
+            outward: string // 不需要登录鉴权的页面
+            SAAS_FEEDBACK: string // 开放平台地址
+            EXTERNAL_LINK: string
+            HEADER_TITLE_NAME: string
+        }
+    }
+    declare function C7NHasModule(dependence: string): boolean
+
+}
+export { };
