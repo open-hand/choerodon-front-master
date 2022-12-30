1c2cefae4c57732b2a63bf58761b9ec7a16aaf35
郑皓天 <haotian.zheng@zknow.com>
2022-12-30 15:36:09 +0800

    [FIX]修改菜单问题

--- react/pages/home-page/components/menu/components/sub-menu/index.tsx
+++ react/pages/home-page/components/menu/components/sub-menu/index.tsx
@@ -209,9 +209,10 @@ const SubMenus:FC<SubMenuProps> = () => {
         subMenuOpenDelay={0.1}
         selectedKeys={[activeMenu?.code].filter(String)}
         // 为了解决收起状态的菜单 所有展开二级菜单都弹出的问题
-        {...isExpanded ? {
-          openKeys: savedOpenKeys,
-        } : {}}
+        openKeys={savedOpenKeys}
+        // {...isExpanded ? {
+        //   openKeys: savedOpenKeys,
+        // } : {}}
         mode="inline"
         onOpenChange={handleOpenChange}
         style={{ overflow: 'hidden overlay' }}
