a67d0c6708264f64894759de10feb02bc5fe0c0b
郑皓天 <273070111@qq.com>
2023-01-06 14:45:17 +0800

    [FIX]修改工作台dashboard调整grid问题

--- react/containers/components/c7n/routes/workBench/components/WorkBenchDashboard/index.js
+++ react/containers/components/c7n/routes/workBench/components/WorkBenchDashboard/index.js
@@ -148,17 +148,23 @@ const WorkBenchDashboard = (props) => {
   });
 
   function onLayoutChange(layouts) {
-    layouts.forEach((card) => {
+    const data = layouts.map((card) => {
+      let newCard = card;
       dashboardDs.forEach((record) => {
         if (record.get('i') === card.i) {
           record.set('x', card.x);
           record.set('y', card.y);
           record.set('w', card.w);
           record.set('h', card.h);
+          newCard = {
+            ...record?.toData(),
+            ...card,
+          };
         }
       });
+      return newCard;
     });
-    // dashboardDs.loadData(layouts);
+    dashboardDs.loadData(data);
   }
 
   function handleDelete(record) {
