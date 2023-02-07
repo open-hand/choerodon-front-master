c0d321a5501d0518d31af9eae7513278e4eada1b
蒋迎宾 <yingbin.jiang@zknow.com>
2023-02-07 11:43:24 +0800

    feat: 动态引入

--- react/pages/dynamicScript.ts
+++ react/pages/dynamicScript.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 // @ts-nocheck
-import React, { useEffect } from 'react';
-import { EventEmitter } from 'events';
+import React, { useEffect } from "react";
+import { EventEmitter } from "events";
 
 const eventBus = new EventEmitter();
 
@@ -11,11 +11,11 @@ window.loadFlag = {};
 function loadComponent(scope, module, onError) {
   return async () => {
     // Initializes the share scope. This fills it with known provided modules from this build and all remotes
-    await __webpack_init_sharing__('default');
+    await __webpack_init_sharing__("default");
     const container = window[scope]; // or get the container somewhere else
     // Initialize the container, it may provide shared modules
     if (!container) {
-      throw new Error('加载了错误的importManifest.js，请检查服务版本');
+      throw new Error("加载了错误的importManifest.js，请检查服务版本");
     }
     try {
       await container.init(__webpack_share_scopes__.default);
@@ -31,15 +31,15 @@ function loadComponent(scope, module, onError) {
   };
 }
 
-const loadScrip = (url, callback) => {
+const loadScript = (url, callback) => {
   // 已加载的资源不再重新加载
-  const scripts = Array.from(document.getElementsByTagName('script'));
-  const isExist = scripts.find(script => script.src === url);
+  const scripts = Array.from(document.getElementsByTagName("script"));
+  const isExist = scripts.find((script) => script.src === url);
   if (isExist) {
     if (
-      window.loadFlag[url]
-      || url?.includes(`localhost:${window.location.port}`)
-      || url?.includes(`127.0.0.1:${window.location.port}`)
+      window.loadFlag[url] ||
+      url?.includes(`localhost:${window.location.port}`) ||
+      url?.includes(`127.0.0.1:${window.location.port}`)
     ) {
       setReady(true);
     } else {
@@ -47,27 +47,20 @@ const loadScrip = (url, callback) => {
         setReady(true);
       });
     }
-    return () => {
-      console.log(`Dynamic Script Removed: ${url}`);
-      // document.head.removeChild(element);
-    };
   }
 
-  const element = document.createElement('script');
+  const element = document.createElement("script");
 
   element.src = url;
-  element.type = 'text/javascript';
-  element.async = true;
+  element.type = "text/javascript";
+  // element.async = true;
 
-  // setReady(false);
-  // setFailed(false);
   window.loadFlag[url] = true;
 
   element.onload = () => {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== "production") {
       console.log(`Dynamic Script Loaded: ${url}`);
     }
-    // setReady(true);
     eventBus.emit(url);
     callback();
   };
@@ -79,28 +72,28 @@ const loadScrip = (url, callback) => {
   };
 
   document.head.appendChild(element);
-}
+};
 
 const useDynamicScript = () => {
   useEffect(() => {
-    Object.keys(env).forEach((i) => {
-      if (i.startsWith('remote_')) {
-        const path: any = i.split('_')[1];
-        const remoteUrl = env[i];
-        loadScrip(remoteUrl.replace('$STATIC_URL', `${env['STATIC_URL']}/${path}`), () => {
-          if (window[path]) {
+    const microValue = env["MICRO_SERVICE"];
+    if (microValue) {
+      microValue.split(",")?.forEach((i) => {
+        const url = env[i] || `${env["STATIC_URL"]}/${i}`;
+        loadScript(`${url}/importManifest.js`, () => {
+          if (window[i]) {
             try {
-              const compo = loadComponent(path, `./${path}`);
-              const inject = loadComponent(path, './install');
-              compo();
+              const compo = loadComponent(i, `./${i}`);
+              const inject = loadComponent(i, "./install");
+              // compo();
               inject();
             } catch (e) {
               console.log(e);
             }
           }
         });
-      }
-    });
+      });
+    }
   }, []);
 };
 
