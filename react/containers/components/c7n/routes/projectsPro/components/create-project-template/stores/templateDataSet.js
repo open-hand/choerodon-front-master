d34fdee601aea99430fdcde3af7a10ff70a69a83
loudingyue <1303785265@qq.com>
2023-05-05 14:42:22 +0800

    [ADD]模板分类API

--- react/containers/components/c7n/routes/projectsPro/components/create-project-template/stores/templateDataSet.js
+++ react/containers/components/c7n/routes/projectsPro/components/create-project-template/stores/templateDataSet.js
@@ -4,9 +4,12 @@ export default ({
   autoCreate: false,
   autoQuery: true,
   transport: {
-    read: {
-      url: `/cbase/choerodon/v1/organizations/${organizationId}/project_template/paging`,
-      method: 'get',
-    },
+    read: ({ params, data }) => ({
+      url: `/cbase/choerodon/v1/organizations/${organizationId}/project_template_classfication/list`,
+      transformResponse: (res) => {
+        const newRes = JSON.parse(res);
+        return newRes.filter((item) => item.name !== '所有项目模板');
+      },
+    }),
   },
 });
