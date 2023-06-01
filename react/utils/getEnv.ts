74af19ed53718ddc044709281eccdf06b2dcd7b3
林岩芳 <yanfang.lin@zknow.com>
2023-06-01 18:54:24 +0800

    [ADD] 添加downloadFileRedirect方法

--- react/utils/getEnv.ts
+++ react/utils/getEnv.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 
-export default function getEnv(valueName: string, defaultValue: any) {
+export default function getEnv(valueName: string, defaultValue?: any) {
   // @ts-ignore
   if (typeof window !== 'undefined' && window._env_ && window._env_[valueName]) {
     // @ts-ignore
